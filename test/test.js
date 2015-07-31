a626bac221827eb75f1a7b771187ed27284f4994
Victor Gartvich <victor@revsw.com>
2015-07-31 01:31:11 +0000

    More code

--- test/test.js
+++ test/test.js
@@ -22,10 +22,12 @@ var qaUserWithUserPerm = 'qa_user_with_user_perm@revsw.com',
 
 describe('Rev API', function() {
 
-  var adminToken = '';
-  var userToken = '';
-  var userCompanyId = '';
-  var domainConfigJson = {};
+  var adminToken = '',
+    userToken = ''.
+    userCompanyId = '',
+    testDomainId,
+    testDomain = 'qa-api-test-domain.revsw.net',
+    domainConfigJson = {};
 
 
   it('should receive 404 on wrong API path', function(done) {
@@ -100,7 +102,9 @@ describe('Rev API', function() {
       });
   });
 
-  it('should be able to get a domains list as user with Admin permissions', function(done) {
+  // Testing domain-related calls
+
+  it('should get a domains list as user with Admin permissions', function(done) {
     request(testAPIUrl)
       .get('/v1/domains')
       .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
@@ -109,8 +113,38 @@ describe('Rev API', function() {
         if (err) {
           throw err;
         }
+
         var response_json = JSON.parse(res.text);
+
         response_json.length.should.be.above(0);
+        for (var i=0; i < response_json.length; i++ ) {
+          response_json[i].companyId.should.be.a.String();
+          response_json[i].name.should.be.a.String();
+          response_json[i].id.should.be.a.String();
+          response_json[i].sync_status.should.be.a.String();
+          if ( response_json[i].name === testDomain ) {
+            testDomainId = response_json[i].id;
+          }
+        }
+        testDomainId.should.be.a.String();
+        done();
+      });
+  });
+
+
+  it('should get domain configuration for test domain', function(done) {
+    request(testAPIUrl)
+      .get('/v1/domains/' + testDomainId)
+      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+
+        var response_json = JSON.parse(res.text);
+        response_json.id.should.be.equal(testDomainId);
+        response_json.name.should.be.equal(testDomain);
         done();
       });
   });
@@ -291,6 +325,22 @@ describe('Rev API Reseller User', function() {
       });
   });
 
+  it('should fail to delete account 55ba46a67957012304a49d0f belonging to another user', function(done) {
+    request(testAPIUrl)
+      .delete('/v1/accounts/55ba46a67957012304a49d0f')
+      .auth(qaUserWithResellerPerm, qaUserWithResellerPermPassword)
+      .expect(400)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.error.should.be.equal('Bad Request');
+        response_json.message.should.be.equal('Account not found');
+        done();
+      });
+  });
+
 
 });
 
@@ -300,10 +350,35 @@ describe('Rev API Admin User', function() {
     userId = '',
     myCompanyId = [],
     myDomains = [],
-    testUser = 'api-qa-user-' + Date().now + '@revsw.com',
+    testUser = 'api-qa-user-' + Date.now() + '@revsw.com',
+    testUserId,
     testPassword = 'password1',
     newTestPassword = 'password2',
     testUserProfile = {};
+
+  var newUserJson = {
+  'firstname': 'API QA User',
+  'lastname': 'With Admin Perm',
+  'email': 'deleteme111@revsw.com',
+  'companyId': [
+    '55b6ff6a7957012304a49d04'
+  ],
+  'domain': [
+    'qa-api-test-domain.revsw.net'
+  ],
+
+  'theme': 'light',
+  'role': 'admin',
+'password': 'password1',
+  'access_control_list': {
+    'readOnly': false,
+    'test': true,
+    'configure': true,
+    'reports': true,
+    'dashBoard': true
+  }
+  };
+
 
 
   it('should be denied access to /v1/accounts functions', function(done) {
@@ -378,7 +453,6 @@ describe('Rev API Admin User', function() {
         response_json.role.should.be.equal('admin');
         response_json.email.should.be.equal(qaUserWithAdminPerm);
         response_json.user_id.should.be.equal(userId);
-        response_json.password.should.not.be.equal(qaUserWithAdminPermPassword);
         done();
       });
   });
@@ -400,7 +474,28 @@ describe('Rev API Admin User', function() {
       });
   });
 
-  it('should create a new user account with Admin permissions' + testUser, function(done) {
+  it('should fail to create a new user account using empty Json', function(done) {
+    request(testAPIUrl)
+      .post('/v1/users')
+      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .send( {} )
+      .expect(400)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.statusCode.should.be.equal(400);
+        response_json.error.should.be.equal('Bad Request');
+        response_json.message.should.be.equal('child "email" fails because ["email" is required]');
+        done();
+      });
+  });
+
+  it('should create a new user account ' + testUser, function(done) {
+    newUserJson.email = testUser;
+    newUserJson.companyId = myCompanyId;
+    newUserJson.domain = myDomains;
     request(testAPIUrl)
       .post('/v1/users')
       .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
@@ -411,16 +506,192 @@ describe('Rev API Admin User', function() {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        response_json.role.should.be.equal('admin');
-        response_json.email.should.be.equal(qaUserWithAdminPerm);
-        response_json.user_id.should.be.equal(userId);
-        response_json.password.should.not.be.equal(qaUserWithAdminPermPassword);
-        testUserProfile = response_json;
+        response_json.statusCode.should.be.equal(200);
+        response_json.message.should.be.equal('Successfully created new user');
+        response_json.object_id.should.be.a.String();
+        testUserId = response_json.object_id;
         done();
       });
   });
 
 
-});
+  it('should read back the configuration of freshly created user ' + testUser, function(done) {
+    request(testAPIUrl)
+      .get('/v1/users/' + testUserId)
+      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.user_id.should.be.equal(testUserId);
+        response_json.updated_at.should.be.a.String();
+        response_json.created_at.should.be.a.String();
 
+        var verifyUserJson = response_json;
+        delete verifyUserJson.created_at;
+        delete verifyUserJson.updated_at;
+        delete verifyUserJson.user_id;
+        delete newUserJson.password;
+        verifyUserJson.should.be.eql(newUserJson);
+        done();
+      });
+  });
+
+
+  it('should get a list of domains using freshly created user ' + testUser, function(done) {
+    request(testAPIUrl)
+      .get('/v1/domains')
+      .auth(testUser, testPassword)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.length.should.be.above(0);
+        done();
+      });
+  });
+
+  it('should change the password for new user account ' + testUser, function(done) {
+    request(testAPIUrl)
+      .put('/v1/users/' + testUserId)
+      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .send( { password: newTestPassword } )
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.statusCode.should.be.equal(200);
+        response_json.message.should.be.equal('Successfully updated the user');
+        done();
+      });
+  });
+
+  it('should get a list of domains using freshly created user ' + testUser +' and new password', function(done) {
+    request(testAPIUrl)
+      .get('/v1/domains')
+      .auth(testUser, newTestPassword)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.length.should.be.above(0);
+        done();
+      });
+  });
+
+  it('should delete test user account ' + testUser, function(done) {
+    request(testAPIUrl)
+      .delete('/v1/users/' + testUserId)
+      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.statusCode.should.be.equal(200);
+        response_json.message.should.be.equal('Successfully deleted the user');
+        done();
+      });
+  });
+
+  it('should create new user account ' + testUser + ' without specifying companyId and domain', function(done) {
+    newUserJson.email = testUser;
+    delete newUserJson.companyId;
+    delete newUserJson.domain;
+    newUserJson.password = testPassword;
+    request(testAPIUrl)
+      .post('/v1/users')
+      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .send(newUserJson)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.statusCode.should.be.equal(200);
+        response_json.message.should.be.equal('Successfully created new user');
+        response_json.object_id.should.be.a.String();
+        testUserId = response_json.object_id;
+        done();
+      });
+  });
+
+  it('should read back the configuration of freshly created user ' + testUser + ' and verify companyId and domain attributes', function(done) {
+
+
+  var newUserJson = {
+  'firstname': 'API QA User',
+  'lastname': 'With Admin Perm',
+  'email': 'deleteme111@revsw.com',
+  'companyId': [
+    '55b6ff6a7957012304a49d04'
+  ],
+  'domain': [
+    'qa-api-test-domain.revsw.net'
+  ],
+
+  'theme': 'light',
+  'role': 'admin',
+'password': 'password1',
+  'access_control_list': {
+    'readOnly': false,
+    'test': true,
+    'configure': true,
+    'reports': true,
+    'dashBoard': true
+  }
+  };
+
+    newUserJson.email = testUser;
+
+    request(testAPIUrl)
+      .get('/v1/users/' + testUserId)
+      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.user_id.should.be.equal(testUserId);
+        response_json.updated_at.should.be.a.String();
+        response_json.created_at.should.be.a.String();
+
+        var verifyUserJson = response_json;
+        delete verifyUserJson.created_at;
+        delete verifyUserJson.updated_at;
+        delete verifyUserJson.user_id;
+        delete newUserJson.password;
+        verifyUserJson.should.be.eql(newUserJson);
+        done();
+      });
+  });
+
+  it('should delete test user account ' + testUser, function(done) {
+    request(testAPIUrl)
+      .delete('/v1/users/' + testUserId)
+      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        response_json.statusCode.should.be.equal(200);
+        response_json.message.should.be.equal('Successfully deleted the user');
+        done();
+      });
+  });
+
+});
 
