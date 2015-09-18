9f8a24b82a2997a348a208ac35594b0b5a1ed5c6
FENIX <fenix123fly@gmail.com>
2015-09-18 04:46:26 +0300

    upd audit handlers

--- test/test-audit.js
+++ test/test-audit.js
@@ -21,27 +21,42 @@
 'use strict';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-var should = require('should-http');
-var request = require('supertest');
-var agent = require('supertest-as-promised');
 
-var express = require('express');
-var fs = require('fs');
-var https = require('https');
-var sleep = require('sleep');
-var utils = require('../lib/utilities.js');
+var request = require('supertest');
+var should  = require('should-http');
+
 var config = require('config');
 
 var qaUserWithUserPerm         = 'qa_user_with_user_perm@revsw.com',
     qaUserWithUserPermPassword = 'password1';
 var qaUserWithAdminPerm         = 'api_qa_user_with_admin_perm@revsw.com',
     qaUserWithAdminPermPassword = 'password1';
+var wrongUser         = 'test@test.com',
+    wrongUserPassword = 'test123456';
 
 var testAPIUrl = ( process.env.API_QA_URL ) ? process.env.API_QA_URL : 'https://localhost:' + config.get('service.https_port');
 
 describe('Rev API /v1/activity call', function() {
 
-  it('should return back audit detailed log', function(done) {
+  it('should return 401 fail', function(done) {
+    request(testAPIUrl)
+      .get('/v1/activity')
+      .auth(wrongUser, wrongUserPassword)
+      .expect(401)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        /*var response = JSON.parse(res.text);
+        console.log(response);*/
+
+        //console.log(response_json);
+        //response_json.target_object.email.should.be.equal(qaUserWithAdminPerm);
+        done();
+      });
+  });
+
+  /*it('should return back audit detailed log', function(done) {
     request(testAPIUrl)
       .get('/v1/activity')
       .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
@@ -50,14 +65,17 @@ describe('Rev API /v1/activity call', function() {
         if (err) {
           throw err;
         }
-        var response_json = JSON.parse(res.text);
-        console.log(response_json);
-        response_json.email.should.be.equal(qaUserWithAdminPerm);
-        response_json.updated_at.should.be.a.String();
-        response_json.created_at.should.be.a.String();
+        var response_array = JSON.parse(res.text);
+
+        for(var key in response_array) {
+          console.log(response_array[key]);
+        }
+
+        //console.log(response_json);
+        //response_json.target_object.email.should.be.equal(qaUserWithAdminPerm);
         done();
       });
-  });
+  });*/
 
   /*it('should return back user details for user with Admin role', function(done) {
     request(testAPIUrl)
