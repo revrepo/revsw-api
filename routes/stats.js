6470964251ec1f02ea422965388cbf89e7a31d50
DmitryM <dmitry@mainlywrenches.co>
2015-11-06 02:12:39 +0400

    RTT reports added to stats

--- routes/stats.js
+++ routes/stats.js
@@ -25,6 +25,7 @@ var Joi = require('joi');
 var getStats = require('../handlers/getStats');
 var getTopObjects = require('../handlers/getTopObjects');
 var getTopReports = require('../handlers/getTopReports');
+var RTTReports = require('../handlers/getRTTReports');
 
 var routeModels = require('../lib/routeModels');
 
@@ -135,5 +136,35 @@ module.exports = [
         }
       }
     }
+  },
+
+  {
+    method: 'GET',
+    path: '/v1/stats/rtt/{domain_id}',
+    config: {
+      auth: {
+        scope: [ 'user', 'admin', 'reseller' ]
+      },
+      handler: RTTReports.getRTTReports,
+      description: 'Get RTT stats for a domain',
+      tags: ['api'],
+      plugins: {
+        'hapi-swagger': {
+          responseMessages: routeModels.standardHTTPErrors
+        }
+      },
+      validate: {
+        params: {
+          domain_id: Joi.objectId().required().description('Domain ID')
+        },
+        query: {
+          from_timestamp: Joi.string().description('Report period start timestamp (defaults to one hour ago from now)'),
+          to_timestamp: Joi.string().description('Report period end timestamp (defaults to now)'),
+          count: Joi.number().integer().min(1).max(250).description('Number of entries to report (default to 30)'),
+          report_type: Joi.string().required().valid ( 'country', 'device', 'os' ).description('Type of requested report (defaults to "country")'),
+        }
+      }
+    }
   }
+
 ];
