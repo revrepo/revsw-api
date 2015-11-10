c968d11086d2cf8aa9ac5d73a161fdc22dfda399
DmitryM <dmitry@mainlywrenches.co>
2015-11-10 05:22:42 +0400

    default and particular timeouts are set to 120s

--- handlers/getRTTReports.js
+++ handlers/getRTTReports.js
@@ -172,7 +172,7 @@ exports.getRTTReports = function(request, reply) {
       elasticSearch.getClientURL().search({
         index: indicesList,
         ignoreUnavailable: true,
-        timeout: 60,
+        timeout: 120000,
         body: requestBody
       }).then(function(body) {
         if ( !body.aggregations ) {
--- handlers/getTopReports.js
+++ handlers/getTopReports.js
@@ -176,7 +176,7 @@ exports.getTopReports = function(request, reply) {
       elasticSearch.getClientURL().search({
         index: indicesList,
         ignoreUnavailable: true,
-        timeout: 60,
+        timeout: 120000,
         body: requestBody
       }).then(function(body) {
         if ( !body.aggregations ) {
