22498b5abe37fc87c41a1f751a20f867dfac6ffd
Vladislav Uspensky <alhkras@gmail.com>
2015-11-27 19:56:37 +0700

    Add activity log tests

--- test/rest_api/functional/domainConfigs.js
+++ test/rest_api/functional/domainConfigs.js
@@ -19,15 +19,18 @@
 var config = require('config');
 var API = require('./../common/api');
 var DataProvider = require('./../common/providers/data');
-
+var should = require('chai').should;
 var async = require('async');
 
-var testDomainID = '';
+var initialDomainID = '';
 var prerequisiteAccountID = '';
+var initialDomain = {};
+var userId = '';
+var testDomain = {};
 
-xdescribe('Domain configs functional test', function () {
+describe('Domain configs functional test', function () {
 
-  this.timeout(config.api.request.maxTimeout);
+  this.timeout(200000);
   var resellerUser = config.api.users.reseller;
 
   API.session.setCurrentUser(resellerUser);
@@ -36,24 +39,33 @@ xdescribe('Domain configs functional test', function () {
 
     API.resources.accounts
       .createOneAsPrerequisite(DataProvider.generateAccount())
-      .then(function (res){
+      .then(function (res) {
         prerequisiteAccountID = res.body.object_id;
-        API.resources.domainConfigs.config
-          .createOneAsPrerequisite(DataProvider
-            .generateInitialDomainConfig(prerequisiteAccountID))
-          .then(function (response) {
-            testDomainID = response.body.object_id;
-          })
-          .finally(function (){
-            done();
-          });
+        initialDomain = DataProvider
+          .generateInitialDomainConfig(prerequisiteAccountID);
+      })
+      .then(function (res) {
+          API.resources.domainConfigs.config
+            .createOneAsPrerequisite(initialDomain)
+            .then(function (response) {
+              initialDomainID = response.body.object_id;
+            })
+            .then(function () {
+              API.resources.users.myself
+                .getAll()
+                .then(function (res) {
+                  userId = res.body.user_id;
+                });
+            })
+            .finally(function (){
+              done();
+            });
       });
   });
 
   after(function (done) {
     API.session.setCurrentUser(resellerUser);
     API.resources.domainConfigs.config.deleteAllPrerequisites(done);
-    done();
   });
 
   describe('Update functions', function () {
@@ -62,7 +74,7 @@ xdescribe('Domain configs functional test', function () {
       function(done){
       API.resources.domainConfigs.config
         .update(
-          testDomainID,
+          initialDomainID,
           DataProvider
             .generateInvalidDomainConfig(prerequisiteAccountID)
         )
@@ -73,7 +85,7 @@ xdescribe('Domain configs functional test', function () {
       function(done){
         API.resources.domainConfigs.verify
           .update(
-            testDomainID,
+            initialDomainID,
             DataProvider
               .generateInvalidDomainConfig(prerequisiteAccountID)
           )
@@ -83,14 +95,14 @@ xdescribe('Domain configs functional test', function () {
       ' and global status Modified after update', function(done){
       API.resources.domainConfigs.config
         .update(
-          testDomainID,
+          initialDomainID,
           DataProvider
             .generateFullDomainConfig(prerequisiteAccountID,'-UPDATED')
         )
         .expect(200)
         .then(function (){
           API.resources.domainConfigs.status
-            .getOne(testDomainID)
+            .getOne(initialDomainID)
             .expect(200, {
               staging_status: 'InProgress',
               global_status: 'Modified'
@@ -101,7 +113,7 @@ xdescribe('Domain configs functional test', function () {
       ' as Published in 120 sec after update', function (done) {
       API.resources.domainConfigs.config
         .update(
-          testDomainID,
+          initialDomainID,
           DataProvider
             .generateFullDomainConfig(prerequisiteAccountID,'-UPDATED')
         )
@@ -116,7 +128,7 @@ xdescribe('Domain configs functional test', function () {
           async.eachSeries(a, function(n, callback) {
             setTimeout( function() {
               API.resources.domainConfigs.status
-                .getOne(testDomainID)
+                .getOne(initialDomainID)
                 .expect(200)
                 .end(function (err, res) {
                   if (err) {
@@ -147,7 +159,7 @@ xdescribe('Domain configs functional test', function () {
       ' after update with publish flag', function (done) {
       API.resources.domainConfigs.publish
         .update(
-          testDomainID,
+          initialDomainID,
           DataProvider
             .generateFullDomainConfig(prerequisiteAccountID,'-UPDATED')
         )
@@ -162,7 +174,7 @@ xdescribe('Domain configs functional test', function () {
           async.eachSeries(a, function(n, callback) {
             setTimeout( function() {
               API.resources.domainConfigs.status
-                .getOne(testDomainID)
+                .getOne(initialDomainID)
                 .expect(200)
                 .end(function (err, res) {
                   if (err) {
@@ -188,5 +200,115 @@ xdescribe('Domain configs functional test', function () {
           });
         });
     });
+
+  });
+
+  describe('Activity log check', function () {
+    this.timeout(200000);
+
+    it('should get correct activity log after Update action',
+      function (done) {
+      API.resources.domainConfigs.config
+        .update(
+          initialDomainID,
+          DataProvider
+            .generateFullDomainConfig(prerequisiteAccountID,'-ACTIVITY')
+        )
+        .expect(200)
+        .then(function () {
+          API.resources.activity
+            .getAll({domain_id: initialDomainID})
+            .expect(function (res){
+              res.body.data[0].activity_target.should.equal('domain');
+              res.body.data[0].activity_type.should.equal('modify');
+              res.body.data[0].user_id.should.equal(userId);
+              res.body.data[0].target_name.should
+                .equal(initialDomain.domain_name);
+
+            })
+            .end(done);
+
+        });
+    });
+    it('should get correct activity log after Publish action', function (done) {
+      API.resources.domainConfigs.publish
+        .update(
+          initialDomainID,
+          DataProvider
+            .generateFullDomainConfig(prerequisiteAccountID,'-ACTIVITY')
+        )
+        .expect(200)
+        .then(function () {
+          API.resources.activity
+            .getAll({domain_id: initialDomainID})
+            .expect(function (res){
+              res.body.data[0].activity_target.should.equal('domain');
+              res.body.data[0].activity_type.should.equal('publish');
+              res.body.data[0].user_id.should.equal(userId);
+              res.body.data[0].target_name.should
+                .equal(initialDomain.domain_name);
+            })
+            .end(done);
+
+        });
+    });
+    describe('Add and delete actions', function () {
+
+      beforeEach(function (done) {
+       testDomain = DataProvider
+         .generateInitialDomainConfig(prerequisiteAccountID);
+        done();
+      });
+      afterEach(function (done) {
+         testDomain = {};
+        done();
+      });
+      it('should get correct activity log after Add action', function (done) {
+        API.resources.domainConfigs.config
+          .createOne(testDomain)
+          .expect(200)
+          .then(function (res) {
+            var object_id = res.body.object_id;
+            API.resources.activity
+              .getAll({domain_id: initialDomainID})
+              .expect(function (res){
+                res.body.data[0].activity_target.should.equal('domain');
+                res.body.data[0].activity_type.should.equal('add');
+                res.body.data[0].user_id.should.equal(userId);
+                res.body.data[0].target_name.should
+                  .equal(testDomain.domain_name);
+              })
+              .then(function () {
+                API.resources.domainConfigs.config
+                  .deleteOne(object_id)
+                  .expect(200)
+                  .end(done);
+              });
+          });
+      });
+      it('should get correct activity log after Delete action', function (done) {
+        API.resources.domainConfigs.config
+          .createOne(testDomain)
+          .expect(200)
+          .then(function (response) {
+            API.resources.domainConfigs.config
+              .deleteOne(response.body.object_id)
+              .expect(200)
+              .then(function () {
+                API.resources.activity
+                  .getAll({domain_id: initialDomainID})
+                  .expect(function (res) {
+                    res.body.data[0].activity_target.should.equal('domain');
+                    res.body.data[0].activity_type.should.equal('delete');
+                    res.body.data[0].user_id.should.equal(userId);
+                    res.body.data[0].target_name.should
+                      .equal(testDomain.domain_name);
+                  })
+                  .end(done);
+              });
+          });
+      });
+    });
   });
 });
+
