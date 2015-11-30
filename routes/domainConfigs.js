e1333f08a83e73fc236e2ab2933f7e60e54e0f77
Victor Gartvich <victor@revsw.com>
2015-11-30 19:26:32 +0000

    Added support for origin_http_keepalive_enabled domain configuration attribute

--- routes/domainConfigs.js
+++ routes/domainConfigs.js
@@ -224,6 +224,7 @@ module.exports = [
             js_choice           : Joi.string().valid('off', 'low', 'medium', 'high').required(),
             css_choice          : Joi.string().valid('off', 'low', 'medium', 'high').required(),
             origin_http_keepalive_ttl:  Joi.number().integer(),
+            origin_http_keepalive_enabled: Joi.boolean()
           }).required(),
           rev_component_bp : Joi.object({
             end_user_response_headers: Joi.array().items({
