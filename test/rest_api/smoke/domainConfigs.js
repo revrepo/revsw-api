32cf4a6ebbff6dd6f25ddc516b17f672b59e3144
Leo <leo.fcx@gmail.com>
2015-12-03 20:55:32 -0400

    Adding smoke tests for domain-config

--- test/rest_api/smoke/domainConfigs.js
+++ test/rest_api/smoke/domainConfigs.js
@@ -18,147 +18,197 @@
 
 var config = require('config');
 var API = require('./../common/api');
-var DataProvider = require('./../common/providers/data');
+var DomainConfigsDP = require('./../common/providers/data/domainConfigs');
 
+describe('Smoke check', function () {
 
+  // Changing default mocha's timeout (Default is 2 seconds).
+  this.timeout(config.get('api.request.maxTimeout'));
 
-xdescribe('Domain configs smoke check', function () {
+  var account;
+  var firstDc;
+  var firstFdc;
+  var secondDc;
+  var reseller = config.get('api.users.reseller');
 
-  var testDomainID = '';
-  var prerequisiteAccountID = '';
-  var initialDomain = {};
+  before(function (done) {
+    API.helpers
+      .authenticateUser(reseller)
+      .then(function () {
+        return API.helpers.accounts.createOne();
+      })
+      .then(function (newAccount) {
+        account = newAccount;
+        return API.helpers.domainConfigs.createOne(account.id);
+      })
+      .then(function (domainConfig) {
+        firstDc = domainConfig;
+      })
+      .then(done)
+      .catch(done);
+  });
 
-  this.timeout(300000);
-  var resellerUser = config.api.users.reseller;
-
-  API.session.setCurrentUser(resellerUser);
+  after(function (done) {
+    API.helpers
+      .authenticateUser(reseller)
+      // TODO: BUG? Cannot delete DomainConfig right after updating it.
+      //.then(function () {
+      //  return API.resources.domainConfigs.deleteOne(firstDc.id);
+      //})
+      .then(function () {
+        return API.resources.accounts.deleteAllPrerequisites(done);
+      })
+      .catch(done);
+  });
 
   describe('Domain configs resource', function () {
-    this.timeout(300000);
 
-
-    before(function (done) {
-      this.timeout(300000);
-
-      API.resources.accounts
-        .createOneAsPrerequisite(DataProvider.generateOne())
-        .then(function (res) {
-          prerequisiteAccountID = res.body.object_id;
-          initialDomain = DataProvider
-            .generateInitialDomainConfig(prerequisiteAccountID);
-        })
-        .then(function (res) {
-          API.resources.domainConfigs.config
-            .createOne(initialDomain)
-            .expect(200, function (err, response) {
-              testDomainID = response.body.object_id;
-              done();
-            });
-        });
+    beforeEach(function (done) {
+      done();
     });
 
-    after(function (done) {
-      this.timeout(300000);
-      API.session.setCurrentUser(resellerUser);
-      API.resources.domainConfigs.config
-        .deleteOne(testDomainID)
-        .expect(200)
-        .then(function () {
-          API.resources.domainConfigs.config.deleteAllPrerequisites(done);
-        });
-
+    afterEach(function (done) {
+      done();
     });
 
-    it('should return 200 status code when getting a list of domains',
+    it('should return success response code when getting a list of domains',
       function (done) {
-      this.timeout(300000);
-      API.resources.domainConfigs.config
-        .getAll()
-        .expect(200)
-        .end(done);
-    });
+        API.helpers
+          .authenticateUser(reseller)
+          .then(function () {
+            API.resources.domainConfigs
+              .getAll()
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
 
-    it('should return a response when creating a new domain',
+    it('should return success response code when creating a new domain',
       function (done) {
-      this.timeout(300000);
-      API.resources.domainConfigs.config
-        .createOne(DataProvider
-          .generateInitialDomainConfig(prerequisiteAccountID))
-        .expect(200)
-        .then(function (response) {
-          API.resources.domainConfigs.config
-            .deleteOne(response.body.object_id)
-            .expect(200)
-            .end(done);
+        secondDc = DomainConfigsDP.generateOne(account.id);
+        API.helpers
+          .authenticateUser(reseller)
+          .then(function () {
+            API.resources.domainConfigs
+              .createOne(secondDc)
+              .expect(200)
+              .then(function (response) { // This is needed for the next tests
+                secondDc.id = response.body.object_id;
+                done();
+              })
+              .catch(done);
+          })
+          .catch(done);
+      });
 
-        });
-    });
-
-    it('should return a response when getting a specific domain',
+    it('should return success response code when getting a specific domain',
       function (done) {
-      this.timeout(300000);
-      API.resources.domainConfigs.config
-        .getOne(testDomainID)
-        .expect(200)
-        .end(done);
-    });
+        API.helpers
+          .authenticateUser(reseller)
+          .then(function () {
+            API.resources.domainConfigs
+              .getOne(firstDc.id)
+              .expect(200)
+              .then(function (response) { // This is needed for the next tests
+                firstFdc = response.body;
+                done();
+              })
+              .catch(done);
+          })
+          .catch(done);
+      });
 
-    it('should return a response when updating a domain', function (done) {
-      this.timeout(300000);
-      API.resources.domainConfigs.config
-        .update(
-          testDomainID,
-          DataProvider
-          .generateFullDomainConfig(prerequisiteAccountID, '-UPDATED')
-        )
-        .expect(200)
-        .end(done);
-    });
+    it('should return success response code when updating a domain',
+      function (done) {
+        firstFdc.origin_host_header = 'UPDATED-' + firstFdc.origin_host_header;
+        firstFdc.origin_server = 'UPDATED-' + firstFdc.origin_server;
+        delete firstFdc.domain_name;
+        delete firstFdc.cname;
+        API.helpers
+          .authenticateUser(reseller)
+          .then(function () {
+            API.resources.domainConfigs
+              .update(firstDc.id, firstFdc)
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
 
-    it('should return a response when deleting a domain', function (done) {
-      this.timeout(300000);
-      API.resources.domainConfigs.config
-        .createOne(DataProvider
-          .generateInitialDomainConfig(prerequisiteAccountID))
-        .then(function (res) {
-          API.resources.domainConfigs.config
-            .deleteOne(res.body.object_id)
-            .expect(200)
-            .end(done);
-        });
-    });
+    it('should return success response code when updating a domain to ' +
+      '`verify only`',
+      function (done) {
+        firstFdc.origin_host_header = 'VERIFY-' + firstFdc.origin_host_header;
+        firstFdc.origin_server = 'VERIFY-' + firstFdc.origin_server;
+        API.helpers
+          .authenticateUser(reseller)
+          .then(function () {
+            API.resources.domainConfigs
+              .update(firstDc.id, firstFdc, {options: 'verify_only'})
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
 
-    it('should return a response on config status request', function (done) {
-      this.timeout(300000);
-      API.resources.domainConfigs.status
-        .getOne(testDomainID)
-        .expect(200)
-        .end(done);
-    });
+    it('should return success response code when updating a domain to ' +
+      '`publish`',
+      function (done) {
+        firstFdc.origin_host_header = 'PUBLISH-' + firstFdc.origin_host_header;
+        firstFdc.origin_server = 'PUBLISH-' + firstFdc.origin_server;
+        API.helpers
+          .authenticateUser(reseller)
+          .then(function () {
+            API.resources.domainConfigs
+              .update(firstDc.id, firstFdc, {options: 'publish'})
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
 
-    it('should respond to PUT request with verify_only flag', function (done){
-      this.timeout(300000);
-      API.resources.domainConfigs.verify
-        .update(
-          testDomainID,
-          DataProvider
-            .generateFullDomainConfig(prerequisiteAccountID)
-        )
-        .expect(200)
-        .end(done);
-    });
+    it('should return success response code when getting the status of ' +
+      'existing domain config',
+      function (done) {
+        API.helpers
+          .authenticateUser(reseller)
+          .then(function () {
+            API.resources.domainConfigs
+              .status(secondDc.id)
+              .getOne()
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
 
-    it('should respond to PUT request with publish flag', function (done){
-      this.timeout(300000);
-      API.resources.domainConfigs.publish
-        .update(
-          testDomainID,
-          DataProvider
-            .generateFullDomainConfig(prerequisiteAccountID)
-        )
-        .expect(200)
-        .end(done);
-    });
+    it('should return success response code when getting versions of ' +
+      'existing domain config',
+      function (done) {
+        API.helpers
+          .authenticateUser(reseller)
+          .then(function () {
+            API.resources.domainConfigs
+              .versions(secondDc.id)
+              .getAll()
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
 
+    it('should return success response code when deleting a domain',
+      function (done) {
+        API.helpers
+          .authenticateUser(reseller)
+          .then(function () {
+            API.resources.domainConfigs
+              .deleteOne(secondDc.id)
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
   });
 });
