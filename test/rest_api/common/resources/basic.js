0cf4c78373a97c5ec7e0eb261afbbf11c0576752
leo-fcx <leo.fcx@gmail.com>
2015-12-21 14:27:51 -0400

    Not deleting prerequisites

--- test/rest_api/common/resources/basic.js
+++ test/rest_api/common/resources/basic.js
@@ -340,7 +340,7 @@ var BasicResource = function (data) {
      * @returns {object} the supertest-as-promised instance
      */
     _resource.deleteAllPrerequisites = function (done) {
-      //return done();/*
+      return done();/*
       return this.deleteMany(_cache)
         .then(function () {
           // What to do in case a pre-requisite is deleted successfully?
@@ -349,7 +349,7 @@ var BasicResource = function (data) {
         .catch(function () {
           // What to do in case a pre-requisite is NOT deleted successfully?
           return done();
-        });
+        });*/
     };
   }
 
