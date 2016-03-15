87ec30b48f2b0a1bf5d0f786e3938710b00ad29e
Victor Gartvich <victor@revsw.com>
2016-03-15 06:53:15 +0000

    Bugfix in user audit record in signup procedure

--- handlers/signup.js
+++ handlers/signup.js
@@ -152,7 +152,7 @@ exports.signup = function(req, reply) {
               ip_address: utils.getAPIUserRealIP(req),
               datetime: Date.now(),
               user_type: 'user',
-              account_id: user.companyId,
+              account_id: user.companyId[0],
               activity_type: 'add',
               activity_target: 'user',
               target_id: user.user_id,
@@ -199,7 +199,7 @@ exports.resetToken = function(req, reply) {
       user_id: user.user_id,
       user_name: user.email,
       user_type: 'user',
-      account_id: result.companyId,
+      account_id: result.companyId[0],
       activity_type: 'modify',
       activity_target: 'user',
       target_id: result.user_id,
@@ -241,7 +241,7 @@ exports.verify = function(req, reply) {
       token: '',
       verified: true
     };
-    var companyId = _.clone(user.companyId);
+    var companyId = _.clone(user.companyId[0]);
     delete user.companyId;
     delete user.password;
     //@todo UPDATE ANYTHING ELSE ?
