5d1c148f802b0a1ff8d6de036f08ebd717ce3a19
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2016-03-22 00:20:15 +0700

    Create Dashboards API

---
+++ handlers/dashboards.js
@@ -0,0 +1,152 @@
+/*************************************************************************
+ *
+ * REV SOFTWARE CONFIDENTIAL
+ *
+ * [2013] - [2016] Rev Software, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Rev Software, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Rev Software, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Rev Software, Inc.
+ */
+
+/*jslint node: true */
+'use strict';
+
+var mongoose = require('mongoose');
+var boom = require('boom');
+var utils = require('../lib/utilities.js');
+var renderJSON = require('../lib/renderJSON');
+var mongoConnection = require('../lib/mongoConnections');
+var publicRecordFields = require('../lib/publicRecordFields');
+
+var Dashboard = require('../models/Dashboard');
+var dashboard = new Dashboard(mongoose, mongoConnection.getConnectionPortal());
+
+exports.getDashboards = function getDashboards(request, reply) {
+  var user_id = request.auth.credentials.user_id;
+  dashboard.getDashboardsByUseID(user_id, function(error, listOfDashboards) {
+    if (error) {
+      return reply(boom.badImplementation('Failed to read dashboards list from the DB'));
+    }
+    var dashboards_list = publicRecordFields.handle(listOfDashboards, 'dashboards');
+    renderJSON(request, reply, error, dashboards_list);
+  });
+};
+
+exports.getDashboard = function getDashboard(request, reply) {
+  var dashboard_id = request.params.dashboard_id;
+
+  dashboard.get({
+    _id: dashboard_id
+  }, function(error, result) {
+    if (error) {
+      return reply(boom.badImplementation('Dashboard::getDashboard: Failed to get a dashboar' +
+        ' Dashboard ID: ' + dashboard_id));
+    }
+
+    if (!result || !utils.checkUserAccessPermissionToDashboard(request, result)) {
+      return reply(boom.badRequest('Dashboard ID not found'));
+    }
+
+    result = publicRecordFields.handle(result, 'dashboard');
+    renderJSON(request, reply, error, result);
+
+  });
+};
+
+exports.createDashboard = function createDashboard(request, reply) {
+  var newDashboard = request.payload;
+  newDashboard.user_id = request.auth.credentials.user_id;
+
+  dashboard.add(newDashboard, function(error, result) {
+    if (error || !result) {
+      return reply(boom.badImplementation('Failed to add new dashboard ' + newDashboard.title));
+    }
+
+    result = publicRecordFields.handle(result, 'dashboard');
+
+    var statusResponse;
+    if (result) {
+      statusResponse = {
+        statusCode: 200,
+        message: 'Successfully created new dashboard',
+        object_id: result.id
+      };
+
+    }
+    renderJSON(request, reply, error, statusResponse);
+  });
+};
+
+exports.deleteDashboard = function(request, reply) {
+  var dashboard_id = request.params.dashboard_id;
+  dashboard.get({
+    _id: dashboard_id
+  }, function(error, result) {
+
+    if (error) {
+      return reply(boom.badImplementation('Error retrieving Dashboard with id ' + dashboard_id));
+    }
+
+    if (!result || !utils.checkUserAccessPermissionToDashboard(request, result)) {
+      return reply(boom.badRequest('Dashboard ID not found'));
+    }
+
+    dashboard.remove({
+      _id: dashboard_id
+    }, function(error) {
+
+      if (error) {
+        return reply(boom.badImplementation('Error removing the dashboard'));
+      }
+
+      var statusResponse;
+      statusResponse = {
+        statusCode: 200,
+        message: 'Successfully deleted the Dashboard'
+      };
+      // TODO: add AuditLogger ???
+      renderJSON(request, reply, error, statusResponse);
+    });
+  });
+};
+
+exports.updateDashboard = function(request, reply) {
+  var updatedData = request.payload;
+  var id = request.params.dashboard_id;
+
+  dashboard.get({
+    _id: id
+  }, function(error, result) {
+
+    if (error) {
+      return reply(boom.badImplementation('Failed to verify Dashboard ' + id));
+    }
+
+    if (!result || !utils.checkUserAccessPermissionToDashboard(request, result)) {
+      return reply(boom.badRequest('Dashboard not found'));
+    }
+
+    updatedData.id = id;
+    dashboard.update(updatedData, function(error, result) {
+      if (error) {
+        return reply(boom.badImplementation('Failed to update Dashboard ' + id));
+      }
+
+      var statusResponse = {
+        statusCode: 200,
+        message: 'Successfully updated the Dashboard'
+      };
+      // TODO: add AuditLogger ???
+      renderJSON(request, reply, error, statusResponse);
+    });
+
+  });
+};
---
+++ routes/dashboards.js
@@ -0,0 +1,156 @@
+/*************************************************************************
+ *
+ * REV SOFTWARE CONFIDENTIAL
+ *
+ * [2013] - [2016] Rev Software, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Rev Software, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Rev Software, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Rev Software, Inc.
+ */
+
+/*jslint node: true */
+
+'use strict';
+
+var Joi = require('joi');
+
+var getDashboarsList = require('../handlers/getCountriesList');
+var dashboars = require('../handlers/dashboards');
+var routeModels = require('../lib/routeModels');
+
+module.exports = [{
+  method: 'GET',
+  path: '/v1/dashboards',
+  config: {
+    auth: {
+      scope: ['user', 'admin', 'reseller', 'revadmin']
+    },
+    handler: dashboars.getDashboards,
+    description: 'Get a list of dashboards',
+    tags: ['api', 'dashboars'],
+    plugins: {
+      'hapi-swagger': {
+        responseMessages: routeModels.standardHTTPErrors
+      }
+    },
+    response: {
+      schema: routeModels.listOfDashboardsModel
+    }
+  }
+}, {
+  method: 'POST',
+  path: '/v1/dashboards',
+  config: {
+    auth: {
+      scope: ['user_rw', 'admin_rw', 'reseller_rw', 'revadmin_rw']
+    },
+    handler: dashboars.createDashboard,
+    description: 'Create a new dashboard ',
+    tags: ['api', 'dashboars'],
+    plugins: {
+      'hapi-swagger': {
+        responseMessages: routeModels.standardHTTPErrors
+      }
+    },
+    validate: {
+      options: {
+        stripUnknown: true
+      },
+      payload: {
+        title: Joi.string().required().min(1).max(150)
+          .trim().description('Dashboard title for screen display'),
+        structure: Joi.string().max(50).trim().description('Name type dashboard structure'),
+        rows: Joi.array().description('Dashboard rows content ')
+      }
+    },
+    response: {
+      schema: routeModels.statusModel
+    }
+  }
+}, {
+  method: 'GET',
+  path: '/v1/dashboards/{dashboard_id}',
+  config: {
+    auth: {
+      scope: ['user', 'admin', 'reseller', 'revadmin']
+    },
+    handler: dashboars.getDashboard,
+    description: 'Get a dashboard',
+    tags: ['api'],
+    plugins: {
+      'hapi-swagger': {
+        responseMessages: routeModels.standardHTTPErrors
+      }
+    },
+    validate: {
+      params: {
+        dashboard_id: Joi.objectId().required().description('Dashboard ID')
+      }
+    },
+    response: {
+      schema: routeModels.dashboardModel
+    }
+  }
+}, {
+  method: 'PUT',
+  path: '/v1/dashboards/{dashboard_id}',
+  config: {
+    auth: {
+      scope: ['user_rw', 'admin_rw', 'reseller_rw', 'revadmin_rw']
+    },
+    handler: dashboars.updateDashboard,
+    description: 'Update a dashboard',
+    notes: 'Use this function to update dashboard information',
+    tags: ['api'],
+    plugins: {
+      'hapi-swagger': {
+        responseMessages: routeModels.standardHTTPErrors
+      }
+    },
+    validate: {
+      options: {
+        stripUnknown: false // TODO: make true
+      },
+      params: {
+        dashboard_id: Joi.string().required().description('ID of the dashboard to be updated')
+      },
+
+    },
+    response: {
+      schema: routeModels.statusModel
+    }
+  }
+}, {
+  method: 'DELETE',
+  path: '/v1/dashboards/{dashboard_id}',
+  config: {
+    auth: {
+      scope: ['user_rw', 'admin_rw', 'reseller_rw', 'revadmin_rw']
+    },
+    handler: dashboars.deleteDashboard,
+    description: 'Remove a dashboard',
+    notes: 'This function should be used  to delete a dashboard',
+    tags: ['api'],
+    plugins: {
+      'hapi-swagger': {
+        responseMessages: routeModels.standardHTTPErrors
+      }
+    },
+    validate: {
+      params: {
+        dashboard_id: Joi.objectId().required().description('ID of the dashboatd to be deleted')
+      }
+    },
+    response: {
+      schema: routeModels.statusModel
+    }
+  }
+}];
