49a45535460ce46f60881e2c554b16a8d54e17b0
Victor Gartvich <victor@revsw.com>
2016-04-17 23:51:05 +0000

    Added a bash script for submiting of Chargify data

---
+++ utils/submit_chargify_billing_report_for_yesterday.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# The script should be executed once a day only on ONE server in an environment.
+#
+# Configure it as the following in /etc/crontab:
+#
+#     # Run daily usage stats collection script
+#     3 3 * * * root /usr/local/bin/lockrun --lockfile=/var/tmp/submit_billing_report.lockrun -- /opt/revsw-api/utils/submit_chargify_billing_report_for_yesterday.sh
+#
+
+CONFIG_FILE=/etc/default/revsw-api
+
+if [ -f $CONFIG_FILE ]; then
+  . $CONFIG_FILE
+fi
+
+API_DIR=/opt/revsw-api
+
+if [ -d $API_DIR ]; then
+  cd $API_DIR
+fi
+
+nodejs utils/push_usage_data_to_the_billing_system.js --date -12h
