19881aa5dbbbcd3cb026a750fc5c34d38cd4ca03
DmitryM <dmitry@mainlywrenches.co>
2016-05-05 21:03:10 +0400

    qa for usage reports, functional tests correction

--- test/rest_api/suites/functional/usage-report.js
+++ test/rest_api/suites/functional/usage-report.js
@@ -36,8 +36,7 @@ describe('UsageReport Functional check:', function () {
     user = config.get('api.usage_report.user'),
     account_id = config.get('api.usage_report.account_id'),
     domain = config.get('api.usage_report.domain_name'),
-    from = ( new Date( Date.now() - 86400000 ) ).toISOString().slice(0,10),
-    to = new Date().toISOString().slice(0,10),
+    ystrdy = ( new Date( Date.now() - 86400000 ) ).toISOString().slice(0,10),
     estimated = UsageDP.countEstimations(),
     test_data_timespan = 12 * 3600,
     report;
@@ -51,8 +50,8 @@ describe('UsageReport Functional check:', function () {
       .then(function () {
         API.resources.usage_report
           .getAll({
-            from: from,
-            to: to,
+            from: ystrdy,
+            to: ystrdy,
             account_id: account_id
           })
           .expect(200)
@@ -78,6 +77,18 @@ describe('UsageReport Functional check:', function () {
     report.should.be.ok();
   });
 
+  it( 'UsageReport processed non-zero records', function () {
+    report.should.be.ok();
+    report.records_processed.should.be.ok();
+    // console.log( report.records_processed );
+    estimated.total_hits *= report.records_processed;
+    estimated.hits.cache_code *= report.records_processed;
+    estimated.hits.protocol *= report.records_processed;
+    estimated.received_bytes *= report.records_processed;
+    estimated.sent_bytes *= report.records_processed;
+    test_data_timespan *= report.records_processed;
+  });
+
   it( 'UsageReport contains domain(' + domain + ') data', function () {
     report.should.be.ok();
     report.domains_usage.should.have.property( domain );
@@ -87,23 +98,25 @@ describe('UsageReport Functional check:', function () {
   it( 'UsageReport contains correct total hits amount', function () {
     report.count.should.be.equal( estimated.total_hits );
   });
+
   it( 'UsageReport contains correct cache hits amount', function () {
     report.cache_hits.MISS.should.be.equal( estimated.hits.cache_code );
     report.cache_hits.HIT.should.be.equal( estimated.hits.cache_code );
   });
+
   it( 'UsageReport contains correct port hits amount', function () {
     report.port_hits['80'].should.be.equal( estimated.hits.protocol );
     report.port_hits['443'].should.be.equal( estimated.hits.protocol );
   });
+
   it( 'UsageReport contains correct traffic values', function () {
     report.received_bytes.should.be.equal( estimated.received_bytes );
     report.sent_bytes.should.be.equal( estimated.sent_bytes );
   });
+
   it( 'UsageReport contains correct billable bandwidth values', function () {
-    ( Math.abs( report.billable_sent_bps -
-      ( estimated.sent_bytes * 8 / test_data_timespan ) ) < 100 ).should.be.true();
-    ( Math.abs( report.billable_received_bps -
-      ( estimated.received_bytes * 8 / test_data_timespan ) ) < 100 ).should.be.true();
+    report.billable_sent_bps.should.be.approximately( ( estimated.sent_bytes * 8 / test_data_timespan ), 100 );
+    report.billable_received_bps.should.be.approximately( ( estimated.received_bytes * 8 / test_data_timespan ), 100 );
   });
 
 });
