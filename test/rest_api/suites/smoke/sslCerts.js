99e2a451dd230a7d30ddf9a9724019ebb54f3639
Leo <leo.fcx@gmail.com>
2016-05-19 13:59:51 -0400

    More smoke tests for SSL Certs

--- test/rest_api/suites/smoke/sslCerts.js
+++ test/rest_api/suites/smoke/sslCerts.js
@@ -21,60 +21,55 @@ var config = require('config');
 
 var API = require('./../../common/api');
 var DataProvider = require('./../../common/providers/data');
+var SSLCertDataProvider = require('./../../common/providers/data/sslCerts');
 
 describe('Smoke check', function () {
   this.timeout(config.get('api.request.maxTimeout'));
 
-  /*// Generating new `user` data in order to use later in our tests.
-  var userSample = DataProvider.generateUser();
+  // Generating new `user` data in order to use later in our tests.
+  //var userSample = SSLCertDataProvider.generateOne();
+  var sslCert;
+  var accountId;
+
   // Retrieving information about specific user that later we will use for
-  // our API requests.*/
-  var resellerUser = config.get('api.users.revAdmin');
+  // our API requests.
+  var user = config.get('api.users.revAdmin');
 
-  //before(function (done) {
-  //  API.helpers
-  //    .authenticateUser(resellerUser)
-  //    .then(function () {
-  //      API.resources.users
-  //        .createOneAsPrerequisite(userSample)
-  //        .then(function (response) {
-  //          userSample.id = response.body.object_id;
-  //          userSample.name = userSample.email;
-  //
-  //          return API.resources.authenticate.createOne({
-  //            email: userSample.email,
-  //            password: userSample.password
-  //          });
-  //        })
-  //        .then(function (response) {
-  //          userSample.token = response.body.token;
-  //          done();
-  //        })
-  //        .catch(done);
-  //    })
-  //    .catch(done);
-  //});
+  before(function (done) {
+    API.helpers
+      .authenticateUser(user)
+      .then(function () {
+        return API.helpers.sslCerts.createOne();
+      })
+      .then(function (certificate) {
+        sslCert = certificate;
+        accountId = sslCert.account_id;
+        done();
+      })
+      .catch(done);
+  });
 
-  //after(function (done) {
-  //  API.helpers
-  //    .authenticateUser(resellerUser)
-  //    .then(function () {
-  //      API.resources.users.deleteAllPrerequisites(done);
-  //    })
-  //    .catch(done);
-  //});
+  after(function (done) {
+    API.helpers
+      .authenticateUser(user)
+      .then(function () {
+        API.resources.sslCerts.deleteAllPrerequisites(done);
+      })
+      .catch(done);
+  });
 
-  describe('Users resource', function () {
+  describe('SSL Certs resource', function () {
+
     it('should return a response when getting all SSL certs.',
       function (done) {
         API.helpers
-          .authenticateUser(resellerUser)
+          .authenticateUser(user)
           .then(function () {
             API.resources.sslCerts
               .getAll()
               .expect(200)
               .then(function (res) {
-                console.log(res.body);
+                //console.log(res.body);
                 done();
               })
               .catch(done);
@@ -82,31 +77,36 @@ describe('Smoke check', function () {
           .catch(done);
       });
 
-    xit('should return a response when getting specific SSL cert.',
+    it('should return a response when getting specific SSL cert.',
       function (done) {
         API.helpers
-          .authenticateUser(resellerUser)
+          .authenticateUser(user)
           .then(function () {
-            API.resources.users
-              .getOne(userSample.id)
+            API.resources.sslCerts
+              .getOne(sslCert.id)
               .expect(200)
-              .end(done);
+              .then(function (res) {
+                //console.log(res.body);
+                done();
+              })
+              .catch(done);
           })
           .catch(done);
       });
 
-    xit('should return a response when creating specific SSL cert.',
+    it('should return a response when creating specific SSL cert.',
       function (done) {
-        var newUser = DataProvider.generateUser();
+        var newSSLCert = SSLCertDataProvider.generateOne(accountId);
         API.helpers
-          .authenticateUser(resellerUser)
+          .authenticateUser(user)
           .then(function () {
-            API.resources.users
-              .createOne(newUser)
+            API.resources.sslCerts
+              .createOne(newSSLCert)
               .expect(200)
               .then(function (response) {
-                API.resources.users
-                  .deleteOne(response.body.object_id)
+                //console.log(response.body);
+                API.resources.sslCerts
+                  .deleteOne(response.body.id)
                   .end(done);
               })
               .catch(done);
@@ -114,75 +114,63 @@ describe('Smoke check', function () {
           .catch(done);
       });
 
-    xit('should return a response when updating specific SSL cert.',
+    it('should return a response when updating specific SSL cert.',
       function (done) {
-        var newUser = DataProvider.generateUser();
         API.helpers
-          .authenticateUser(resellerUser)
+          .authenticateUser(user)
           .then(function () {
-            API.resources.users
-              .createOneAsPrerequisite(newUser)
-              .then(function (response) {
-                newUser.firstName = 'John';
-                newUser.lastName = 'Doe';
-                API.resources.users
-                  .update(response.body.object_id, newUser)
-                  .expect(200)
-                  .end(done);
+            return API.helpers.sslCerts.createOne();
+          })
+          .then(function (certificate) {
+            var certificateId = certificate.id
+            certificate.cert_name += 'UPDATED';
+            delete certificate.id
+            API.resources.sslCerts
+              .update(certificateId, certificate)
+              .expect(200)
+              .then(function (res) {
+                //console.log(res.body);
+                done();
               })
               .catch(done);
           })
           .catch(done);
       });
 
-    xit('should return a response when deleting a SSL cert.',
-      function (done) {
-      var newUser = DataProvider.generateUser();
-      API.helpers
-        .authenticateUser(resellerUser)
-        .then(function () {
-          API.resources.users
-            .createOneAsPrerequisite(newUser)
-            .then(function (response) {
-              var objectId = response.body.object_id;
-              API.resources.users
-                .deleteOne(objectId)
-                .expect(200)
-                .end(done);
-            })
-            .catch(done);
-        })
-        .catch(done);
-    });
-
-    xit('should return a response when getting my user profile.',
+    it('should return a response when deleting a SSL cert.',
       function (done) {
         API.helpers
-          .authenticateUser(resellerUser)
+          .authenticateUser(user)
           .then(function () {
-            API.resources.users
-              .myself()
-              .getOne()
+            return API.helpers.sslCerts.createOne();
+          })
+          .then(function (certificate) {
+            API.resources.sslCerts
+              .deleteOne(certificate.id)
               .expect(200)
-              .end(done);
+              .then(function (res) {
+                //console.log(res.body);
+                done();
+              })
+              .catch(done);
           })
           .catch(done);
       });
 
-    // ### Spec/test to update a user's password
-    xit('should return a response when updating specific user\'s password.',
+    it('should return a response when getting config-status of a SSL cert.',
       function (done) {
         API.helpers
-          .authenticateUser(userSample)
+          .authenticateUser(user)
           .then(function () {
-            API.resources.users
-              .password()
-              .update(userSample.id, {
-                current_password: userSample.password,
-                new_password: 'secret321'
-              })
+            API.resources.sslCerts
+              .configStatus(sslCert.id)
+              .getAll()
               .expect(200)
-              .end(done);
+              .then(function (res) {
+                //console.log(res.body);
+                done();
+              })
+              .catch(done);
           })
           .catch(done);
       });
