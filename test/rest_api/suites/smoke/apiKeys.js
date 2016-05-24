5456c866506111923b38f1236f3a8d7579e4a69e
Leo <leo.fcx@gmail.com>
2016-05-24 18:16:39 -0400

    Adding initial test for API Keys

---
+++ test/rest_api/suites/smoke/apiKeys.js
@@ -0,0 +1,159 @@
+/*************************************************************************
+ *
+ * REV SOFTWARE CONFIDENTIAL
+ *
+ * [2013] - [2016] Rev Software, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Rev Software, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Rev Software, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Rev Software, Inc.
+ */
+
+// # Smoke check: API Keys
+var config = require('config');
+
+var API = require('./../../common/api');
+var DataProvider = require('./../../common/providers/data');
+var APIKeyDataProvider = require('./../../common/providers/data/apiKeys');
+
+describe('Smoke check', function () {
+  this.timeout(config.get('api.request.maxTimeout'));
+
+  // Shared variables
+  var apiKey;
+  var accountId;
+
+  // Retrieving information about specific user that later we will use for
+  // our API requests.
+  var user = config.get('api.users.revAdmin');
+
+  before(function (done) {
+    API.helpers
+      .authenticateUser(user)
+      .then(function () {
+        return API.helpers.apiKeys.createOne();
+      })
+      .then(function (key) {
+        apiKey = key;
+        accountId = apiKey.account_id;
+        done();
+      })
+      .catch(done);
+  });
+
+  after(function (done) {
+    API.helpers
+      .authenticateUser(user)
+      .then(function () {
+        API.resources.apiKeys.deleteAllPrerequisites(done);
+      })
+      .catch(done);
+  });
+
+  describe('API Keys resource', function () {
+
+    it('should return a success response when getting all API Keys.',
+      function (done) {
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            API.resources.apiKeys
+              .getAll()
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
+
+    it('should return a success response when getting specific API Keys.',
+      function (done) {
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            API.resources.apiKeys
+              .getOne(apiKey.id)
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
+
+    it('should return a success response when creating specific API Key.',
+      function (done) {
+        var newApiKey = APIKeyDataProvider.generateOne(accountId);
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            API.resources.apiKeys
+              .createOne(newApiKey)
+              .expect(200)
+              .then(function (response) {
+                API.resources.apiKeys
+                  .deleteOne(response.body.id)
+                  .end(done);
+              })
+              .catch(done);
+          })
+          .catch(done);
+      });
+
+    it('should return a success response when updating specific AI Key.',
+      function (done) {
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            return API.helpers.apiKeys.createOne();
+          })
+          .then(function (key) {
+            var apiKeyId = key.id;
+            key.cert_name += 'UPDATED';
+            delete key.id;
+
+            API.resources.apiKeys
+              .update(apiKeyId, key)
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
+
+    it('should return a success response when deleting a API Key.',
+      function (done) {
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            return API.helpers.apiKeys.createOne();
+          })
+          .then(function (key) {
+            API.resources.apiKeys
+              .deleteOne(key.id)
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
+
+    //it('should return a success response when getting config-status of a ' +
+    //  'API Key.',
+    //  function (done) {
+    //    API.helpers
+    //      .authenticateUser(user)
+    //      .then(function () {
+    //        API.resources.apiKeys
+    //          .configStatus(apiKey.id)
+    //          .getAll()
+    //          .expect(200)
+    //          .end(done);
+    //      })
+    //      .catch(done);
+    //  });
+  });
+});
+
