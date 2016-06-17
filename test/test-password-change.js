a12ee34e66d128d737ee2368f000148d44c29f1f
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2016-06-18 00:39:15 +0700

    Update test-password-change.js

--- test/test-password-change.js
+++ test/test-password-change.js
@@ -10,35 +10,46 @@ var sleep = require('sleep');
 var utils = require('../lib/utilities.js');
 var config = require('config');
 
-var testAPIUrl = ( process.env.API_QA_URL ) ? process.env.API_QA_URL : 'https://localhost:' + config.get('service.https_port');
-var testAPIUrlHTTP = ( process.env.API_QA_URL_HTTP ) ? process.env.API_QA_URL_HTTP : 'http://localhost:' + config.get('service.http_port');
-var testAPIUrlExpected = ( process.env.API_QA_URL ) ? process.env.API_QA_URL : 'https://localhost:' + config.get('service.http_port');
+var testAPIUrl = (process.env.API_QA_URL) ? process.env.API_QA_URL : 'https://localhost:' + config.get('service.https_port');
+var testAPIUrlHTTP = (process.env.API_QA_URL_HTTP) ? process.env.API_QA_URL_HTTP : 'http://localhost:' + config.get('service.http_port');
+var testAPIUrlExpected = (process.env.API_QA_URL) ? process.env.API_QA_URL : 'https://localhost:' + config.get('service.http_port');
 
 var qaUserWithUserPerm = 'qa_user_with_user_perm@revsw.com',
   qaUserWithAdminPerm = 'api_qa_user_with_admin_perm@revsw.com',
   qaUserWithAdminPermPassword = 'password1',
   qaUserWithRevAdminPerm = 'qa_user_with_rev-admin_perm@revsw.com',
+  qaUserWithRevAdminPermPassword = 'password1',
   qaUserWithResellerPerm = 'api_qa_user_with_reseller_perm@revsw.com',
   qaUserWithResellerPermPassword = 'password1',
   wrongUsername = 'wrong_username@revsw.com',
   wrongPassword = 'we5rsdfsdfs',
-  testDomain = 'qa-api-test-domain.revsw.net';  // this domain should exist in the QA environment
+  testDomain = 'qa-api-test-domain.revsw.net'; // this domain should exist in the QA environment
+
+var userAuthWithAdminPerm = {
+  email: qaUserWithAdminPerm,
+  password: qaUserWithAdminPermPassword
+};
+var userAuthWithRevAdminPerm = {
+  email: qaUserWithRevAdminPerm,
+  password: qaUserWithRevAdminPermPassword
+};
 
 describe('Rev user password change API', function() {
 
   this.timeout(10000);
 
-  var adminToken = '',
-      userToken = '',
-      userCompanyId = '',
-      testDomainId,
-      testDomain = 'qa-api-test-domain.revsw.net',
-      domainConfigJson = {};
+  var jwtTokenWithAdminPerm = '',
+    jwtTokenWithRevAdminPerm = '',
+    userToken = '',
+    userCompanyId = '',
+    testDomainId,
+    testDomain = 'qa-api-test-domain.revsw.net',
+    domainConfigJson = {};
 
   var testUser = 'api-qa-user-' + Date.now() + '@revsw.com',
-      testUserId = '',
-      testPass = 'password1',
-      newTestPass = 'password2';
+    testUserId = '',
+    testPass = 'password1',
+    newTestPass = 'password2';
 
   var changePasswordJson = {
     current_password: testPass,
@@ -60,11 +71,38 @@ describe('Rev user password change API', function() {
       'dashBoard': true
     }
   };
+  before(function(done) {
+    request(testAPIUrl)
+      .post('/v1/authenticate')
+      .send(userAuthWithRevAdminPerm)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        var response_json = JSON.parse(res.text);
+        jwtTokenWithRevAdminPerm = response_json.token;
+
+        request(testAPIUrl)
+          .post('/v1/authenticate')
+          .send(userAuthWithAdminPerm)
+          .expect(200)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            var response_json = JSON.parse(res.text);
+            jwtTokenWithAdminPerm = response_json.token;
+
+            done();
+          });
+      });
+  })
 
   it('should create a new user account ' + testUser, function(done) {
     request(testAPIUrl)
       .post('/v1/users')
-      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .set('Authorization', 'Bearer ' + jwtTokenWithAdminPerm)
       .send(newUserJson)
       .expect(200)
       .end(function(err, res) {
@@ -83,14 +121,14 @@ describe('Rev user password change API', function() {
   it('should find a new record about the addition of new user in logger', function(done) {
     request(testAPIUrl)
       .get('/v1/activity')
-      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .set('Authorization', 'Bearer ' + jwtTokenWithAdminPerm)
       .expect(200)
       .end(function(err, res) {
         if (err) {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        var last_obj      = response_json.data[0];
+        var last_obj = response_json.data[0];
         last_obj.target_id.should.be.equal(testUserId);
         last_obj.activity_type.should.be.equal('add');
         last_obj.activity_target.should.be.equal('user');
@@ -98,61 +136,101 @@ describe('Rev user password change API', function() {
       });
   });
 
-
   it('should fail to change the password for another user', function(done) {
     request(testAPIUrl)
-      .put('/v1/users/password/55f206b68593406503e1fdb6')
-      .auth(testUser, testPass)
-      .send(changePasswordJson)
-      .expect(400)
+      .post('/v1/authenticate')
+      .send({
+        email: testUser,
+        password: testPass
+      })
+      .expect(200)
       .end(function(err, res) {
         if (err) {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        response_json.statusCode.should.be.equal(400);
-        response_json.message.should.be.equal('Cannot update the password of another user');
-        done();
+        var jwtTokenTestUser = response_json.token;
+        request(testAPIUrl)
+          .put('/v1/users/password/55f206b68593406503e1fdb6')
+          .set('Authorization', 'Bearer ' + jwtTokenTestUser)
+          .send(changePasswordJson)
+          .expect(400)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            var response_json = JSON.parse(res.text);
+            response_json.statusCode.should.be.equal(400);
+            response_json.message.should.be.equal('Cannot update the password of another user');
+            done();
+          });
       });
   });
 
-
   it('should fail to change the password with empty JSON', function(done) {
     request(testAPIUrl)
-      .put('/v1/users/password/' + testUserId)
-      .auth(testUser, testPass)
-      .send( {} )
-      .expect(400)
+      .post('/v1/authenticate')
+      .send({
+        email: testUser,
+        password: testPass
+      })
+      .expect(200)
       .end(function(err, res) {
         if (err) {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        response_json.statusCode.should.be.equal(400);
-        done();
+        var jwtTokenTestUser = response_json.token;
+        request(testAPIUrl)
+          .put('/v1/users/password/' + testUserId)
+          .set('Authorization', 'Bearer ' + jwtTokenTestUser)
+          .send({})
+          .expect(400)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            var response_json = JSON.parse(res.text);
+            response_json.statusCode.should.be.equal(400);
+            done();
+          });
       });
   });
 
   it('should fail to change the password with wrong current password', function(done) {
-
     var changePassJsonLocal = {
-    current_password: testPass,
-    new_password: newTestPass
-  };
+      current_password: testPass,
+      new_password: newTestPass
+    };
     changePassJsonLocal.current_password = '234afsdfswsdf';
     request(testAPIUrl)
-      .put('/v1/users/password/' + testUserId)
-      .auth(testUser, testPass)
-      .send( changePassJsonLocal )
-      .expect(400)
+      .post('/v1/authenticate')
+      .send({
+        email: testUser,
+        password: testPass
+      })
+      .expect(200)
       .end(function(err, res) {
         if (err) {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        response_json.statusCode.should.be.equal(400);
-        response_json.message.should.be.equal('The current user password is not correct');
-        done();
+        var jwtTokenTestUser = response_json.token;
+        request(testAPIUrl)
+          .put('/v1/users/password/' + testUserId)
+          .set('Authorization', 'Bearer ' + jwtTokenTestUser)
+          // .auth(testUser, testPass)
+          .send(changePassJsonLocal)
+          .expect(400)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            var response_json = JSON.parse(res.text);
+            response_json.statusCode.should.be.equal(400);
+            response_json.message.should.be.equal('The current user password is not correct');
+            done();
+          });
       });
   });
 
@@ -163,18 +241,32 @@ describe('Rev user password change API', function() {
     };
     changePassJsonLocal.new_password = '234af';
     request(testAPIUrl)
-      .put('/v1/users/password/' + testUserId)
-      .auth(testUser, testPass)
-      .send( changePassJsonLocal )
-      .expect(400)
+      .post('/v1/authenticate')
+      .send({
+        email: testUser,
+        password: testPass
+      })
+      .expect(200)
       .end(function(err, res) {
         if (err) {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        response_json.statusCode.should.be.equal(400);
-//        response_json.message.should.be.equal('The current user password is not correct');
-        done();
+        var jwtTokenTestUser = response_json.token;
+        request(testAPIUrl)
+          .put('/v1/users/password/' + testUserId)
+          .set('Authorization', 'Bearer ' + jwtTokenTestUser)
+          .send(changePassJsonLocal)
+          .expect(400)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            var response_json = JSON.parse(res.text);
+            response_json.statusCode.should.be.equal(400);
+            //        response_json.message.should.be.equal('The current user password is not correct');
+            done();
+          });
       });
   });
 
@@ -184,59 +276,100 @@ describe('Rev user password change API', function() {
       new_password: newTestPass
     };
     request(testAPIUrl)
-      .put('/v1/users/password/' + testUserId)
-      .auth(testUser, testPass)
-      .send( changePasswordJson )
+      .post('/v1/authenticate')
+      .send({
+        email: testUser,
+        password: testPass
+      })
       .expect(200)
       .end(function(err, res) {
         if (err) {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        response_json.statusCode.should.be.equal(200);
-        response_json.message.should.be.equal('Successfully updated the password');
-        done();
+        var jwtTokenTestUser = response_json.token;
+        request(testAPIUrl)
+          .put('/v1/users/password/' + testUserId)
+          .set('Authorization', 'Bearer ' + jwtTokenTestUser)
+          .send(changePasswordJson)
+          .expect(200)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            var response_json = JSON.parse(res.text);
+            response_json.statusCode.should.be.equal(200);
+            response_json.message.should.be.equal('Successfully updated the password');
+            done();
+          });
       });
   });
 
   it('should find a new record about updating password the user in logger', function(done) {
     request(testAPIUrl)
-      .get('/v1/activity')
-      .auth(testUser, newTestPass)
+      .post('/v1/authenticate')
+      .send({
+        email: testUser,
+        password: newTestPass
+      })
       .expect(200)
       .end(function(err, res) {
         if (err) {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        var last_obj      = response_json.data[0];
-        last_obj.target_id.should.be.equal(testUserId);
-        last_obj.activity_type.should.be.equal('modify');
-        last_obj.activity_target.should.be.equal('user');
-        done();
+        var jwtTokenTestUser = response_json.token;
+        request(testAPIUrl)
+          .get('/v1/activity')
+          .set('Authorization', 'Bearer ' + jwtTokenTestUser)
+          .expect(200)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            var response_json = JSON.parse(res.text);
+            var last_obj = response_json.data[1];
+            last_obj.target_id.should.be.equal(testUserId);
+            last_obj.activity_type.should.be.equal('modify');
+            last_obj.activity_target.should.be.equal('user');
+            done();
+          });
       });
   });
 
   it('should get user details using the new password', function(done) {
     request(testAPIUrl)
-      .get('/v1/users/myself')
-      .auth(testUser, newTestPass)
+      .post('/v1/authenticate')
+      .send({
+        email: testUser,
+        password: newTestPass
+      })
       .expect(200)
       .end(function(err, res) {
         if (err) {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        response_json.email.should.be.equal(testUser);
-        done();
+        var jwtTokenTestUser = response_json.token;
+        request(testAPIUrl)
+          .get('/v1/users/myself')
+          .set('Authorization', 'Bearer ' + jwtTokenTestUser)
+          .expect(200)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            var response_json = JSON.parse(res.text);
+            response_json.email.should.be.equal(testUser);
+            done();
+          });
       });
   });
-
 
   it('should delete test user account ' + testUser, function(done) {
     request(testAPIUrl)
       .delete('/v1/users/' + testUserId)
-      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .set('Authorization', 'Bearer ' + jwtTokenWithAdminPerm)
       .expect(200)
       .end(function(err, res) {
         if (err) {
@@ -252,21 +385,18 @@ describe('Rev user password change API', function() {
   it('should find a new record about deleting the user in logger', function(done) {
     request(testAPIUrl)
       .get('/v1/activity')
-      .auth(qaUserWithAdminPerm, qaUserWithAdminPermPassword)
+      .set('Authorization', 'Bearer ' + jwtTokenWithAdminPerm)
       .expect(200)
       .end(function(err, res) {
         if (err) {
           throw err;
         }
         var response_json = JSON.parse(res.text);
-        var last_obj      = response_json.data[0];
+        var last_obj = response_json.data[0];
         last_obj.target_id.should.be.equal(testUserId);
         last_obj.activity_type.should.be.equal('delete');
         last_obj.activity_target.should.be.equal('user');
         done();
       });
   });
-
 });
-
-
