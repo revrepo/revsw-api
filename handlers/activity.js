c506820cdbbf38645729e2f5f7bd6d901009dd27
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2016-07-04 23:26:14 +0700

    Rename parameters in handkers/activity.js

--- handlers/activity.js
+++ handlers/activity.js
@@ -37,27 +37,27 @@ var domainConfigs = new DomainConfig(mongoose, mongoConnection.getConnectionPort
 
 exports.getDetailedAuditInfo = function(request, reply) {
   var requestBody = {};
-  var start_time;
-  var end_time;
+  var startTime;
+  var endTime;
   var targetType = request.query.target_type;
   var targetId = request.query.target_id;
-  var user_id = request.query.user_id ? request.query.user_id : request.auth.credentials.user_id;
-  var account_id;
+  var userId = request.query.user_id ? request.query.user_id : request.auth.credentials.user_id;
+  var accountId;
   if (request.auth.credentials.user_type === 'user') {
-    account_id = request.query.account_id ? request.query.account_id : request.auth.credentials.companyId[0];
+    accountId = request.query.account_id ? request.query.account_id : request.auth.credentials.companyId[0];
   } else {
-    account_id = utils.getAccountID(request, true);
+    accountId = utils.getAccountID(request, true);
   }
 
   if (request.query.user_id) {
-    requestBody['meta.user_id'] = user_id;
+    requestBody['meta.user_id'] = userId;
   }
   if (request.query.account_id) {
-    requestBody['meta.account_id'] = account_id;
+    requestBody['meta.account_id'] = accountId;
   }
 
   if (!request.query.user_id && !request.query.account_id) {
-    requestBody['meta.user_id'] = user_id;
+    requestBody['meta.user_id'] = userId;
   }
 
   var currentUser = null;
@@ -65,9 +65,9 @@ exports.getDetailedAuditInfo = function(request, reply) {
   async.waterfall([
 
     function getUserInfo(cb) {
-      users.getById(user_id, function(err, result) {
+      users.getById(userId, function(err, result) {
         if (err) {
-          cb({ errorCode: 500, err: err, message: 'Failed to retreive user details for ID ' + user_id });
+          cb({ errorCode: 500, err: err, message: 'Failed to retreive user details for ID ' + userId });
           return;
         }
         if (!result) {
@@ -90,7 +90,7 @@ exports.getDetailedAuditInfo = function(request, reply) {
             cb({ errorCode: 400, err: null, message: 'User ID not found' });
             return;
           }
-          if (!utils.checkUserAccessPermissionToAccount(request, account_id)) {
+          if (!utils.checkUserAccessPermissionToAccount(request, accountId)) {
             cb({ errorCode: 400, err: null, message: 'Account ID not found' });
             return;
           }
@@ -104,7 +104,7 @@ exports.getDetailedAuditInfo = function(request, reply) {
             cb({ errorCode: 400, err: null, message: 'User ID not found' });
             return;
           }
-          if (!utils.checkUserAccessPermissionToAccount(request, account_id)) {
+          if (!utils.checkUserAccessPermissionToAccount(request, accountId)) {
             cb({ errorCode: 400, err: null, message: 'Account ID not found' });
             return;
           }
@@ -117,7 +117,7 @@ exports.getDetailedAuditInfo = function(request, reply) {
             cb({ errorCode: 400, err: null, message: 'User ID not found' });
             return;
           }
-          if (!utils.checkUserAccessPermissionToAccount(request, account_id)) {
+          if (!utils.checkUserAccessPermissionToAccount(request, accountId)) {
             cb({ errorCode: 400, err: null, message: 'Account ID not found' });
             return;
           }
@@ -169,12 +169,12 @@ exports.getDetailedAuditInfo = function(request, reply) {
       if (span.error) {
         return reply(boom.badRequest(span.error));
       }
-      start_time = span.start;
-      end_time = span.end;
+      startTime = span.start;
+      endTime = span.end;
 
       requestBody['meta.datetime'] = {
-        '$gte': start_time,
-        '$lte': end_time
+        '$gte': startTime,
+        '$lte': endTime
       };
       // TODO: Need to add proper indexes for the audit collection
       auditevents.detailed(requestBody, function(error, data) {
@@ -184,10 +184,10 @@ exports.getDetailedAuditInfo = function(request, reply) {
         }
         var result = {
           metadata: {
-            user_id: user_id,
-            account_id: account_id,
-            start_time: start_time,
-            end_time: end_time
+            user_id: userId,
+            account_id: accountId,
+            start_time: startTime,
+            end_time: endTime
           },
           data: data
         };
@@ -215,15 +215,15 @@ exports.getDetailedAuditInfo = function(request, reply) {
 // TODO: the Summary function is a mess - need to do a full review
 exports.getSummaryAuditInfo = function(request, reply) {
   var requestBody = {};
-  var start_time;
-  var end_time;
+  var startTime;
+  var endTime;
 
-  var user_id = request.query.user_id ? request.query.user_id : request.auth.credentials.user_id;
+  var userId = request.query.user_id ? request.query.user_id : request.auth.credentials.user_id;
 
   async.waterfall([
 
     function(cb) {
-      users.getById(user_id, function(err, result) {
+      users.getById(userId, function(err, result) {
         if (err || !result) {
           return reply(boom.badRequest('User ID not found'));
         }
@@ -242,7 +242,7 @@ exports.getSummaryAuditInfo = function(request, reply) {
           if (request.query.company_id && !utils.isArray1IncludedInArray2([request.query.company_id], request.auth.credentials.companyId)) {
             return reply(boom.badRequest('Account ID not found'));
           }
-          requestBody['meta.user_id'] = user_id;
+          requestBody['meta.user_id'] = userId;
           break;
 
         case 'admin':
@@ -252,7 +252,7 @@ exports.getSummaryAuditInfo = function(request, reply) {
           if (request.query.company_id && !utils.isArray1IncludedInArray2([request.query.company_id], request.auth.credentials.companyId)) {
             return reply(boom.badRequest('Account ID not found'));
           }
-          requestBody['meta.user_id'] = user_id;
+          requestBody['meta.user_id'] = userId;
           break;
 
         case 'reseller':
@@ -263,7 +263,7 @@ exports.getSummaryAuditInfo = function(request, reply) {
             return reply(boom.badRequest('Account ID not found'));
           }
           if (request.query.user_id) {
-            requestBody['meta.user_id'] = user_id;
+            requestBody['meta.user_id'] = userId;
           }
           break;
       }
@@ -284,22 +284,22 @@ exports.getSummaryAuditInfo = function(request, reply) {
       if (span.error) {
         return reply(boom.badRequest(span.error));
       }
-      start_time = span.start;
-      end_time = span.end;
+      startTime = span.start;
+      endTime = span.end;
 
       requestBody['meta.datetime'] = {
-        '$gte': start_time,
-        '$lte': end_time
+        '$gte': startTime,
+        '$lte': endTime
       };
 
       auditevents.summary(requestBody, function(error, data) {
         var result = {
           metadata: {
-            user_id: user_id,
+            user_id: userId,
             //domain_id  : request.query.domain_id,
             company_id: request.query.company_id ? request.query.company_id : user.companyId,
-            start_time: start_time,
-            end_time: end_time
+            start_time: startTime,
+            end_time: endTime
           },
           data: data
         };
