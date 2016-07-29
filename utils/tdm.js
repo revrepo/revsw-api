7302ce1b7811d822c632a4479fe9d8bb02dd9b94
dmitry_t <dmitry@mainlywrenches.co>
2016-07-29 04:32:53 +0400

    domain aliases tests added

--- utils/tdm.js
+++ utils/tdm.js
@@ -23,11 +23,16 @@
 var promise = require('bluebird');
 // var config = require( 'config' );
 var dp = require( '../test/rest_api/common/providers/statsData.js' );
+var utils = require( '../test/rest_api/common/utils.js' );
+var prefix_lg = '    • ';
+var prefix_sm = '    · ';
+var prefix_cccp = '☭ ';
+
 
 //  CLI -----------------------------
 
 var showHelp = function() {
-  console.log('\n  Test Data Manager - a tool to generate, upload, remove test data from the ES clusters');
+  console.log('\n  ' + prefix_cccp + 'Test Data Manager - a tool to generate, upload, remove test data from the ES clusters');
   console.log('  Usage:');
   console.log('    --gen, --generate :');
   console.log('        generate data' );
@@ -47,11 +52,13 @@ var showHelp = function() {
   console.log('        this message');
   console.log('    -v, --verbose :');
   console.log('        blubbing output');
+  console.log('    --no-colors :');
+  console.log('        non-colored output');
   console.log('    --dry-run :');
   console.log('        show spans and return\n');
 };
 
-var conf = {},
+var conf = { colors: true },
   pars = process.argv.slice(2),
   parslen = pars.length,
   curr_par = false,
@@ -83,6 +90,8 @@ for (var i = 0; i < parslen; ++i) {
     action = 'generate';
   } else if (pars[i] === '-v' || pars[i] === '--verbose') {
     conf.verbose = true;
+  } else if (pars[i] === '--no-colors') {
+    conf.colors = false;
   } else if (pars[i] === '--remove') {
     action = 'remove';
   } else if (pars[i] === '--regenerate') {
@@ -96,6 +105,12 @@ for (var i = 0; i < parslen; ++i) {
   }
 }
 
+if ( conf.colors ) {
+  prefix_lg = '    ' + utils.colored( 'LightBlue', '• ' );
+  prefix_sm = '    ' + utils.colored( 'LightBlue', '· ' );
+  prefix_ussr = utils.colored( 'Red', '☭ ' );
+}
+
 if (action === '') {
   console.error('\n    the action is not specified');
   showHelp();
@@ -106,6 +121,18 @@ if ( ( !conf.to || !conf.from) && action !== 'regenerate' ) {
   console.error( 'insufficient from/to data, meta will be loaded ...' );
   showHelp();
   return;
+}
+
+var logger, sublogger;
+if ( conf.verbose ) {
+  logger = function( msg ) {
+    console.log( prefix_lg + msg );
+  };
+  sublogger = function( msg ) {
+    console.log( prefix_sm + msg );
+  };
+} else {
+  logger = sublogger = function() {};
 }
 
 //  actions --------------------------------------------------------------------------------------//
@@ -125,25 +152,22 @@ if ( action === 'generate' ) {
   }
 
   var DP_ESURL = new dp( conf.from, conf.to );
-  console.log( '    ### start data generation' );
+  console.log( prefix_lg + 'start data generation' );
   DP_ES.generateTestingData('es');
-  if ( conf.verbose ) {
-    console.log( '    ### ES cluster, generation done, ' + DP_ES.options.dataCount + ' records.' );
-  }
+  logger( 'ES cluster, generation done, ' + DP_ES.options.dataCount + ' records.' );
+
   DP_ESURL.generateTestingData('esurl');
-  if ( conf.verbose ) {
-    console.log( '    ### ESURL cluster, generation done, ' + DP_ESURL.options.dataCount + ' records.' );
-  }
-  console.log( '    ### start data uploading' );
+  logger( 'ESURL cluster, generation done, ' + DP_ESURL.options.dataCount + ' records.' );
+  console.log( prefix_lg + 'start data uploading' );
 
   var up = [
-    DP_ESURL.uploadTestingData( 'esurl', conf.verbose ),
-    DP_ES.uploadTestingData( 'es', conf.verbose )
+    DP_ESURL.uploadTestingData( 'esurl', null/*default type*/, sublogger ),
+    DP_ES.uploadTestingData( 'es', null/*default type*/, sublogger )
   ];
 
   return promise.all( up )
     .then( function() {
-      console.log( '    ### done' );
+      console.log( prefix_lg + 'done' );
       process.exit(0);
       return;
     })
@@ -167,20 +191,18 @@ if ( action === 'remove' ) {
   }
 
   var DP_ESURL = new dp( conf.from, conf.to );
-  console.log( '    ### start data removing' );
+  console.log( prefix_lg + 'start data removing' );
 
   var rems = [
-    DP_ES.removeTestingData( 'es', conf.verbose ),
-    DP_ESURL.removeTestingData( 'esurl', conf.verbose )
+    DP_ES.removeTestingData( 'es', null/*default type*/, sublogger ),
+    DP_ESURL.removeTestingData( 'esurl', null/*default type*/, sublogger )
   ];
 
   return promise.all( rems )
     .then( function( num ) {
-      console.log( '    ### done' );
-      if ( conf.verbose ) {
-        console.log( '    ### ES cluster:    ' + num[0] + ' records removed' );
-        console.log( '    ### ESURL cluster: ' + num[1] + ' records removed' );
-      }
+      console.log( prefix_lg + 'done' );
+      logger( 'ES cluster:    ' + num[0] + ' records removed' );
+      logger( 'ESURL cluster: ' + num[1] + ' records removed' );
       process.exit(0);
     })
     .catch( function( e ) {
@@ -197,13 +219,13 @@ if ( action === 'regenerate' ) {
 
   var DP_ESURL = new dp( from, from + ( 2 * day_ms ) /*spans 2 whole days*/ );
   if ( conf.dry ) {
-    console.log( '    ### data clearance span:' );
+    console.log( prefix_lg + 'data clearance span:' );
     console.log( '    From date: ' + ( new Date( DP_ESURL.options.from ) ).toUTCString() );
     console.log( '      To date: ' + ( new Date( DP_ESURL.options.to ) ).toUTCString() );
     console.log( '      Indices: ' + DP_ESURL.options.indicesList + '\n' );
 
     DP_ESURL.autoSpan();
-    console.log( '    ### data generation span:' );
+    console.log( prefix_lg + 'data generation span:' );
     console.log( '    From date: ' + ( new Date( DP_ESURL.options.from ) ).toUTCString() );
     console.log( '      To date: ' + ( new Date( DP_ESURL.options.to ) ).toUTCString() );
     console.log( '      Indices: ' + DP_ESURL.options.indicesList + '\n' );
@@ -212,50 +234,44 @@ if ( action === 'regenerate' ) {
   }
 
   var DP_ES = new dp( from, from + ( 2 * day_ms ) /*spans 2 whole days*/ );
-  console.log( '    ### data clearance span:' );
+  console.log( prefix_lg + 'data clearance span:' );
   console.log( '    From date: ' + ( new Date( DP_ES.options.from ) ).toUTCString() );
   console.log( '      To date: ' + ( new Date( DP_ES.options.to ) ).toUTCString() );
   console.log( '      Indices: ' + DP_ES.options.indicesList + '\n' );
 
-  console.log( '    ### start data removing' );
+  console.log( prefix_lg + 'start data removing' );
   var rems = [
-    DP_ES.removeTestingData( 'es', conf.verbose ),
-    DP_ESURL.removeTestingData( 'esurl', conf.verbose )
+    DP_ES.removeTestingData( 'es', null/*def type*/, sublogger ),
+    DP_ESURL.removeTestingData( 'esurl', null/*def type*/, sublogger )
   ];
 
   return promise.all( rems )
     .then( function( num ) {
-      console.log( '    ### done' );
-      if ( conf.verbose ) {
-        console.log( '    ### ES cluster:    ' + num[0] + ' records removed' );
-        console.log( '    ### ESURL cluster: ' + num[1] + ' records removed' );
-      }
+      console.log( prefix_lg + 'done' );
+      logger( 'ES cluster:    ' + num[0] + ' records removed' );
+      logger( 'ESURL cluster: ' + num[1] + ' records removed' );
       DP_ESURL.autoSpan();
       DP_ES.autoSpan();
-      console.log( '\n    ### data generation span:' );
+      console.log( '\n' + prefix_lg + 'data generation span:' );
       console.log( '    From date: ' + ( new Date( DP_ES.options.from ) ).toUTCString() );
       console.log( '      To date: ' + ( new Date( DP_ES.options.to ) ).toUTCString() );
       console.log( '      Indices: ' + DP_ES.options.indicesList + '\n' );
 
       DP_ES.generateTestingData('es');
-      if ( conf.verbose ) {
-        console.log( '    ### ES cluster, generation done, ' + DP_ES.options.dataCount + ' records.' );
-      }
+      logger( 'ES cluster, generation done, ' + DP_ES.options.dataCount + ' records.' );
       DP_ESURL.generateTestingData('esurl');
-      if ( conf.verbose ) {
-        console.log( '    ### ESURL cluster, generation done, ' + DP_ESURL.options.dataCount + ' records.' );
-      }
-      console.log( '    ### start data uploading' );
+      logger( 'ESURL cluster, generation done, ' + DP_ESURL.options.dataCount + ' records.' );
+      console.log( prefix_lg + 'start data uploading' );
 
       var up = [
-        DP_ESURL.uploadTestingData( 'esurl', conf.verbose ),
-        DP_ES.uploadTestingData( 'es', conf.verbose )
+        DP_ESURL.uploadTestingData( 'esurl', null/*def type*/, sublogger ),
+        DP_ES.uploadTestingData( 'es', null/*def type*/, sublogger )
       ];
 
       return promise.all( up );
     })
     .then( function() {
-      console.log( '    ### done' );
+      console.log( prefix_lg + 'done' );
       process.exit(0);
       return;
     })
