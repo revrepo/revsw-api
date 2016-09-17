14258f930e16545350da40f6aa69fbd707e22599
Victor Gartvich <victor@revsw.com>
2016-09-17 01:55:29 +0000

    [ci skip] Increased timeout for old API key tests

--- test/test-api-keys.js
+++ test/test-api-keys.js
@@ -27,7 +27,7 @@ var userAuthWithUserPerm = {
 };
 describe('Rev API keys', function() {
 
-  this.timeout(60000);
+  this.timeout(180000);
 
   var myCompanyId = '55b6ff6a7957012304a49d04';
   var myDomains = [];
