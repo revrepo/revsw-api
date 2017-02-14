f329cf52dd7377409842320d798bf6c0cd821455
Leo <leo.fcx@gmail.com>
2017-02-13 21:27:03 -0400

    Adding initial crud tests for apikeys

--- test/rest_api/suites/crud/apiKeys.js
+++ test/rest_api/suites/crud/apiKeys.js
@@ -16,3 +16,131 @@
  * from Rev Software, Inc.
  */
 
+require('should-http');
+
+// # CRUD check: API Keys
+var config = require('config');
+
+var API = require('./../../common/api');
+var APIKeyDataProvider = require('./../../common/providers/data/apiKeys');
+
+describe('Smoke check', function () {
+  this.timeout(config.get('api.request.maxTimeout'));
+
+  // Shared variables
+  var apiKey;
+  var accountId;
+
+  // Retrieving information about specific user that later we will use for
+  // our API requests.
+  var user = config.get('api.users.revAdmin');
+
+  before(function (done) {
+    API.helpers
+      .authenticateUser(user)
+      .then(function () {
+        return API.helpers.apiKeys.createOne();
+      })
+      .then(function (key) {
+        apiKey = key;
+        accountId = apiKey.account_id;
+        done();
+      })
+      .catch(done);
+  });
+
+  after(function (done) {
+    done();
+  });
+
+  describe('API Keys resource', function () {
+
+    it('should allow to get all API Keys.',
+      function (done) {
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            return API.resources.apiKeys
+              .getAll()
+              .expect(200);
+          })
+          .then(function (response) {
+            var apiKeys = response.body;
+            apiKeys.should.not.be.empty();
+            apiKeys.length.should.greaterThan(0);
+            done();
+          })
+          .catch(done);
+      });
+
+    it('should allow to get specific API Key.',
+      function (done) {
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            return API.resources.apiKeys
+              .getOne(apiKey.id)
+              .expect(200);
+          })
+          .then(function (response) {
+            response.body.id.should.equal(apiKey.id);
+            done();
+          })
+          .catch(done);
+      });
+
+    it('should allow to create specific API Key.',
+      function (done) {
+        var newApiKey = APIKeyDataProvider.generateOne(accountId);
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            return API.resources.apiKeys
+              .createOne(newApiKey)
+              .expect(200);
+          })
+          .then(function (response) {
+            response.body.statusCode.should.equal(200);
+            response.body.object_id.should.not.be.undefined();
+            done();
+          })
+          .catch(done);
+      });
+
+    it('should allow to update specific API Key.',
+      function (done) {
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            return API.helpers.apiKeys.createOne();
+          })
+          .then(function (key) {
+            var apiKeyId = key.id;
+            var updatedKey = APIKeyDataProvider
+              .generateCompleteOne(key.account_id);
+            API.resources.apiKeys
+              .update(apiKeyId, updatedKey)
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
+
+    xit('should allow to delete an API Key.',
+      function (done) {
+        API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            return API.helpers.apiKeys.createOne();
+          })
+          .then(function (key) {
+            API.resources.apiKeys
+              .deleteOne(key.id)
+              .expect(200)
+              .end(done);
+          })
+          .catch(done);
+      });
+  });
+});
+
