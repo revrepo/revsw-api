f5b4a9a1c951e3c3b1b7e055dd8f6c522d0a931e
Leo <leo.fcx@gmail.com>
2017-04-04 16:41:23 -0400

    HealthCheck resource object

---
+++ test/rest_api/common/resources/healthCheck.js
@@ -0,0 +1,35 @@
+/*************************************************************************
+ *
+ * REV SOFTWARE CONFIDENTIAL
+ *
+ * [2013] - [2017] Rev Software, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Rev Software, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Rev Software, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Rev Software, Inc.
+ */
+
+// # HealthCheck Resource object
+
+var BasicResource = require('./basic');
+var Constants = require('./../../common/constants');
+var Methods = Constants.API.METHODS;
+
+var resourceConfig = {
+  name: 'healthCheck',
+  path: '/healthcheck',
+  methods: [
+    Methods.READ_ALL
+  ]
+};
+
+// Creating new instance of BaseResource which is going to represent the API
+// `health-check resource`
+module.exports = new BasicResource(resourceConfig);
