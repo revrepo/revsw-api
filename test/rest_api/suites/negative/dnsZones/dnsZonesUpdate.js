c28347e93554c723eebb01dae52d95e7ad42e047
Leo <leo.fcx@gmail.com>
2017-05-19 20:20:17 -0400

    Update dnsZone specs for negative check

---
+++ test/rest_api/suites/negative/dnsZones/dnsZonesUpdate.js
@@ -0,0 +1,140 @@
+/*************************************************************************
+ *
+ * REV SOFTWARE CONFIDENTIAL
+ *
+ * [2013] - [2017] Rev Software, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Rev Software, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Rev Software, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Rev Software, Inc.
+ */
+
+require('should-http');
+var config = require('config');
+var Constants = require('./../../../common/constants');
+var API = require('./../../../common/api');
+var Utils = require('./../../../common/utils');
+var DNSZonesDP = require('./../../../common/providers/data/dnsZones');
+
+describe('DNS Zones resource: pre-requisites', function () {
+
+  // Changing default mocha's timeout (Default is 2 seconds).
+  this.timeout(config.get('api.request.maxTimeout'));
+
+  var dataTypes = [
+    Constants.API.TEST_DATA_TYPES.BOGUS,
+    Constants.API.TEST_DATA_TYPES.INVALID,
+    Constants.API.TEST_DATA_TYPES.NIL
+  ];
+  var users = [
+    config.get('api.users.reseller')
+  ];
+  var dnsZones = {};
+
+  dataTypes.forEach(function (type) {
+
+    before(function (done) {
+
+      Utils.forEach(users, function (user) {
+        return API.helpers
+          .authenticateUser(user)
+          .then(function () {
+            return API.helpers.accounts.createOne();
+          })
+          .then(function (account) {
+            return API.helpers.dnsZones.create(account.id);
+          })
+          .then(function (key) {
+            dnsZones[user.role] = key;
+          });
+      }, done);
+    });
+
+    before(function (done) {
+
+      /**
+       * Generates Spec name for the given data.
+       *
+       * @param type
+       * @param field
+       * @param value
+       * @returns {string}
+       */
+      var getSpecName = function (type, field, value) {
+        return 'should return error response when updating `DNS Zone` with ' +
+          type + ' `' + field + '`: ' + value;
+      };
+      /**
+       * Generates Spec function for the given data.
+       *
+       * @param user
+       * @param model
+       * @returns {Function}
+       */
+      var getSpecFn = function (user, field, model) {
+        var fieldName = Utils.getLastKeyFromPath(field);
+        return function (done) {
+          API.helpers
+            .authenticateUser(user)
+            .then(function () {
+              var dnsZone = dnsZones[user.role];
+              console.log(model);
+              return API.resources.dnsZones.update(dnsZone.id, model);
+            })
+            .then(function (res) {
+              res.body.statusCode.should.equal(400);
+              res.body.message.should.containEql(fieldName);
+              done();
+            })
+            .catch(done);
+        };
+      };
+
+      DNSZonesDP.DataDrivenHelper.payload.genToUpdate(type,
+        function (err, data) {
+
+          users.forEach(function (user) {
+            describe('Boundary check', function () {
+              // Changing default mocha's timeout (Default is 2 seconds).
+              this.timeout(config.get('api.request.maxTimeout'));
+              describe('DNS Zones resource', function () {
+                describe('With user: ' + user.role, function () {
+                  describe('Update with `' + type + '` data', function () {
+
+                    data.forEach(function (dnsZone) {
+                      var field = dnsZone.field;
+                      var model = dnsZone.model;
+                      var value = Utils.getValueByPath(model, field);
+
+                      if (value === undefined || value === null) {
+                        return;
+                      }
+
+                      it(
+                        getSpecName(type, field, value),
+                        getSpecFn(user, field, model)
+                      );
+                    });
+                  });
+                });
+              });
+            });
+          });
+          done();
+        });
+    });
+
+    it('Generating `update-' + type + '-data` specs ...', function () {
+      // Do not remove this spec as it is required to auto-generate other specs.
+      Object.keys(dnsZones).length.should.be.above(0); // Pre-requisites check
+    });
+  });
+});
+
