58690018b97e87e974977341e29fb9dd68912453
Yan Pylypenko <metallica.yp@gmail.com>
2017-12-01 20:10:00 +0200

    Create and Debug Azure Tests

--- test/rest_api/suites/negative/azureNotAllowedUser.js
+++ test/rest_api/suites/negative/azureNotAllowedUser.js
@@ -141,6 +141,179 @@ describe('Negative check', function () {
               })
               .catch(done);
           });
+
+        it('should return `Unauthorized` response when create a subscription with user-role user.', 
+          function (done) {
+            var subscription = AzureDP.generateTwo().subscription_id;
+            var state = AzureDP.generate();
+            API.helpers
+              .authenticateUser(user)
+              .then(function () {
+                API.resources.azure
+                  .subscriptions()
+                  .update(subscription, state)
+                  .expect(401)
+                  .end(done);
+              })
+              .catch(done);
+          });
+
+        it('should return `Unauthorized` response when create a resource with user-role user.', 
+          function (done) {
+            var provider = AzureDP.generateOne().provider;
+            var subscription = AzureDP.generateOne().subscription_id;
+            var resourceGroupName = AzureDP.generateOne().resource_group_name;
+            var resourceName = AzureDP.generateOne().resource_name;
+            var location = AzureDP.generateLocation();
+            API.helpers
+              .authenticateUser(user)
+              .then(function () {
+                API.resources.azure
+                  .subscriptions()
+                  .resourceGroups(subscription)
+                  .providers(resourceGroupName)
+                  .accounts(provider) 
+                  .update(resourceName, location)
+                  .expect(401)
+                  .end(done);
+              })
+              .catch(done);
+          });
+
+        it('should return `Unauthorized` response when move a resource with user-role user.', 
+          function (done) {
+            var subscription = AzureDP.generateOne().subscription_id;
+            var resourceGroupName = AzureDP.generateOne().resource_group_name;
+            API.helpers
+              .authenticateUser(user)
+              .then(function () {
+                API.resources.azure
+                  .subscriptions()
+                  .resourceGroups(subscription)
+                  .moveResources(resourceGroupName)
+                  .createOne()
+                  .expect(401)
+                  .end(done);
+              })
+              .catch(done);
+          });
+
+        it('should return `Unauthorized` response when create a list secrets with user-role user.', 
+          function (done) {
+            var provider = AzureDP.generateOne().provider;
+            var subscription = AzureDP.generateOne().subscription_id;
+            var resourceGroupName = AzureDP.generateOne().resource_group_name;
+            var resourceName = AzureDP.generateOne().resource_name;
+            API.helpers
+              .authenticateUser(user)
+              .then(function () {
+                API.resources.azure
+                  .subscriptions()
+                  .resourceGroups(subscription)
+                  .providers(resourceGroupName)
+                  .accounts(provider) 
+                  .listSecrets(resourceName)
+                  .createOne()
+                  .expect(401)
+                  .end(done);
+              })
+              .catch(done);
+          });
+
+        it('should return `Unauthorized` response when getting all operations with user-role user.', 
+          function (done) {
+            var provider = AzureDP.generateOne().provider;
+            API.helpers
+              .authenticateUser(user)
+              .then(function () {
+                API.resources.azure
+                  .providers()
+                  .operations(provider)
+                  .getAll()
+                  .expect(401)
+                  .end(done);
+              })
+              .catch(done);
+          });
+
+        it('should return `Unauthorized` response when update Communication Preference with user-role user.', 
+          function (done) {
+            var provider = AzureDP.generateOne().provider;
+            var subscription = AzureDP.generateOne().subscription_id;
+            API.helpers
+              .authenticateUser(user)
+              .then(function () {
+                API.resources.azure
+                  .subscriptions()
+                  .providers(subscription)
+                  .updateCommunicationPreference(provider) 
+                  .createOne()
+                  .expect(401)
+                  .end(done);
+              })
+              .catch(done);
+          });
+
+        it('should return `Unauthorized` response get list Communication Preference with user-role user.', 
+          function (done) {
+            var provider = AzureDP.generateOne().provider;
+            var subscription = AzureDP.generateOne().subscription_id;
+            API.helpers
+              .authenticateUser(user)
+              .then(function () {
+                API.resources.azure
+                  .subscriptions()
+                  .providers(subscription)
+                  .listCommunicationPreference(provider) 
+                  .createOne()
+                  .expect(401)
+                  .end(done);
+              })
+              .catch(done);
+          });
+
+        it('should return `Unauthorized` response when regenerate key with user-role user.', 
+          function (done) {
+            var provider = AzureDP.generateOne().provider;
+            var subscription = AzureDP.generateOne().subscription_id;
+            var resourceGroupName = AzureDP.generateOne().resource_group_name;
+            var resourceName = AzureDP.generateOne().resource_name;
+            API.helpers
+              .authenticateUser(user)
+              .then(function () {
+                API.resources.azure
+                  .subscriptions()
+                  .resourceGroups(subscription)
+                  .providers(resourceGroupName)
+                  .accounts(provider) 
+                  .regenerateKey(resourceName)
+                  .createOne()
+                  .expect(401)
+                  .end(done);
+              })
+              .catch(done);
+          });
+
+        it('should return `Unauthorized` response when delete a resource with user-role user.', 
+          function (done) {
+            var provider = AzureDP.generateOne().provider;
+            var subscription = AzureDP.generateOne().subscription_id;
+            var resourceGroupName = AzureDP.generateOne().resource_group_name;
+            var resourceName = AzureDP.generateOne().resource_name;
+            API.helpers
+              .authenticateUser(user)
+              .then(function () {
+                API.resources.azure
+                  .subscriptions()
+                  .resourceGroups(subscription)
+                  .providers(resourceGroupName)
+                  .accounts(provider) 
+                  .deleteOne(resourceName)
+                  .expect(401)
+                  .end(done);
+              })
+              .catch(done);
+          });
       });
     });
   });
