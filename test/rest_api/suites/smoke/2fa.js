e90c355c164ebfdcd55e7af19c4029be9ab97a04
AsherMoshav <asher@mediadome.co.il>
2018-01-12 16:32:50 +0200

    2FA smoke tests extended

--- test/rest_api/suites/smoke/2fa.js
+++ test/rest_api/suites/smoke/2fa.js
@@ -28,12 +28,18 @@ describe('Smoke check:', function () {
 
   // Changing default mocha's timeout (Default is 2 seconds).
   this.timeout(config.get('api.request.maxTimeout'));
-  var revAdminCredentials = config.api.users.revAdmin;
-
+  
   var userRolesList = [
     'user',
     'admin',
     'reseller'
+  ];
+
+  var users = [
+    config.get('api.users.reseller'),
+    config.get('api.users.revAdmin'),
+    config.get('api.apikeys.admin'),
+    config.get('api.apikeys.reseller')
   ];
 
   before(function (done) {
@@ -44,153 +50,77 @@ describe('Smoke check:', function () {
     done();
   });
 
-  userRolesList.forEach(function (roleName) {
-    describe('2fa resource for user with role "' + roleName + '"', function () {
-      var user, accountForUsers;
-      before(function (done) {
-        API.helpers
-          .authenticate(revAdminCredentials)
-          .then(function () {
-            return HelpersAPI.accounts.createCompleteOne();
-          })
-          .then(function (newAccount) {
-            accountForUsers = newAccount;
-          })
-          .then(done)
-          .catch(done);
-      });
+  users.forEach(function (user) {
+    describe('With ' + user.role, function () {
+      userRolesList.forEach(function (role) {
+        describe('2fa resource for user with role "' + role + '"', function () {
+          var testUser, accountForUsers;
+          before(function (done) {
+            API.helpers
+              .authenticate(user)
+              .then(function () {
+                if (user.key) {
+                  return user.account;
+                } else {
+                  return HelpersAPI.accounts.createCompleteOne();
+                }
+              })
+              .then(function (newAccount) {
+                accountForUsers = newAccount;
+              })
+              .then(done)
+              .catch(done);
+          });
 
-      beforeEach(function (done) {
-        API.helpers
-          .authenticate(revAdminCredentials)
-          .then(function () {
-            var newUser = DataProvider.generateUser(roleName);
-            newUser.companyId = [accountForUsers.id];
-            newUser.domain = [];
-            return API.helpers.users.create(newUser);
-          })
-          .then(function (newUser) {
-            user = newUser;
-          })
-          .then(done)
-          .catch(done);
-      });
+          beforeEach(function (done) {
+            API.helpers
+              .authenticate(user)
+              .then(function () {
+                var newUser = DataProvider.generateUser(role);
+                newUser.companyId = [accountForUsers.id];
+                newUser.domain = [];
+                return API.helpers.users.create(newUser);
+              })
+              .then(function (newUser) {
+                testUser = newUser;
+              })
+              .then(done)
+              .catch(done);
+          });
 
-      afterEach(function (done) {
-        done();
-      });
+          afterEach(function (done) {
+            done();
+          });
 
-      it('should return success response when initializing 2fa for specific user',
-        function (done) {
-          API.helpers
-            .authenticateUser(user)
-            .then(function () {
-              API.resources.twoFA
-                .init()
-                .getOne()
-                .expect(200)
-                .end(done);
-            })
-            .catch(done);
-        });
-
-      it('should return success response when enabling 2fa for specific user',
-        function (done) {
-          API.helpers
-            .authenticateUser(user)
-            .then(function () {
-              API.resources.twoFA
-                .init()
-                .getOne()
-                .expect(200)
-                .then(function (res) {
-                  var key = res.body.base32;
-                  var oneTimePassword = TwoFADP.generateOneTimePassword(key);
+          it('should return success response when initializing 2fa for specific user',
+            function (done) {
+              API.helpers
+                .authenticateUser(testUser)
+                .then(function () {
                   API.resources.twoFA
-                    .enable()
-                    .createOne(oneTimePassword)
+                    .init()
+                    .getOne()
                     .expect(200)
                     .end(done);
                 })
-                .catch(function (err) {
-                  throw new Error(err);
-                  done();
-                });
-            })
-            .catch(done);
-        });
+                .catch(done);
+            });
 
-      it('should return success response when authenticating with a valid OTP',
-        function (done) {
-          API.helpers
-            .authenticateUser(user)
-            .then(function () {
-              API.resources.twoFA
-                .init()
-                .getOne()
-                .expect(200)
-                .then(function (res) {
-                  var key = res.body.base32;
-                  var oneTimePassword = TwoFADP.generateOneTimePassword(key);
+          it('should return success response when enabling 2fa for specific user',
+            function (done) {
+              API.helpers
+                .authenticateUser(testUser)
+                .then(function () {
                   API.resources.twoFA
-                    .enable()
-                    .createOne(oneTimePassword)
+                    .init()
+                    .getOne()
                     .expect(200)
-                    .then(function () {
+                    .then(function (res) {
+                      var key = res.body.base32;
+                      var oneTimePassword = TwoFADP.generateOneTimePassword(key);
                       API.resources.twoFA
-                        .init()
-                        .getOne()
-                        .expect(200)
-                        .then(function (res) {
-                          var key = res.body.base32;
-                          return TwoFADP.generateOneTimePassword(key);
-                        })
-                        .then(function (otp) {
-                          user.oneTimePassword = otp.oneTimePassword;
-                          API.helpers
-                            .authenticateUser(user)
-                            .then(function () {
-                              done();
-                            })
-                            .catch(function (err) {
-                              throw new Error(err);
-                              done();
-                            });
-                        })
-                        .catch(function (err) {
-                          throw new Error(err);
-                          done();
-                        });
-                    });
-                })
-                .catch(function (err) {
-                  throw new Error(err);
-                  done();
-                });
-            })
-            .catch(done);
-        });
-
-      it('should return success response when disabling 2fa for specific user',
-        function (done) {
-          API.helpers
-            .authenticateUser(user)
-            .then(function () {
-              API.resources.twoFA
-                .init()
-                .getOne()
-                .expect(200)
-                .then(function (res) {
-                  var key = res.body.base32;
-                  var oneTimePassword = TwoFADP.generateOneTimePassword(key);
-                  API.resources.twoFA
-                    .enable()
-                    .createOne(oneTimePassword)
-                    .expect(200)
-                    .then(function () {
-                      API.resources.twoFA
-                        .disable()
-                        .createOne(user.id)
+                        .enable()
+                        .createOne(oneTimePassword)
                         .expect(200)
                         .end(done);
                     })
@@ -199,13 +129,97 @@ describe('Smoke check:', function () {
                       done();
                     });
                 })
-                .catch(function (err) {
-                  throw new Error(err);
-                  done();
-                });
-            })
-            .catch(done);
+                .catch(done);
+            });
+
+          it('should return success response when authenticating with a valid OTP',
+            function (done) {
+              API.helpers
+                .authenticateUser(testUser)
+                .then(function () {
+                  API.resources.twoFA
+                    .init()
+                    .getOne()
+                    .expect(200)
+                    .then(function (res) {
+                      var key = res.body.base32;
+                      var oneTimePassword = TwoFADP.generateOneTimePassword(key);
+                      API.resources.twoFA
+                        .enable()
+                        .createOne(oneTimePassword)
+                        .expect(200)
+                        .then(function () {
+                          API.resources.twoFA
+                            .init()
+                            .getOne()
+                            .expect(200)
+                            .then(function (res) {
+                              var key = res.body.base32;
+                              return TwoFADP.generateOneTimePassword(key);
+                            })
+                            .then(function (otp) {
+                              user.oneTimePassword = otp.oneTimePassword;
+                              API.helpers
+                                .authenticateUser(testUser)
+                                .then(function () {
+                                  done();
+                                })
+                                .catch(function (err) {
+                                  throw new Error(err);
+                                  done();
+                                });
+                            })
+                            .catch(function (err) {
+                              throw new Error(err);
+                              done();
+                            });
+                        });
+                    })
+                    .catch(function (err) {
+                      throw new Error(err);
+                      done();
+                    });
+                })
+                .catch(done);
+            });
+
+          it('should return success response when disabling 2fa for specific user',
+            function (done) {
+              API.helpers
+                .authenticateUser(testUser)
+                .then(function () {
+                  API.resources.twoFA
+                    .init()
+                    .getOne()
+                    .expect(200)
+                    .then(function (res) {
+                      var key = res.body.base32;
+                      var oneTimePassword = TwoFADP.generateOneTimePassword(key);
+                      API.resources.twoFA
+                        .enable()
+                        .createOne(oneTimePassword)
+                        .expect(200)
+                        .then(function () {
+                          API.resources.twoFA
+                            .disable()
+                            .createOne(testUser.id)
+                            .expect(200)
+                            .end(done);
+                        })
+                        .catch(function (err) {
+                          throw new Error(err);
+                          done();
+                        });
+                    })
+                    .catch(function (err) {
+                      throw new Error(err);
+                      done();
+                    });
+                })
+                .catch(done);
+            });
         });
+      });
     });
   });
 });
