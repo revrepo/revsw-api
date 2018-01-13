fab3f8cb2cfcce5d7feaba781fad6ccc357928bb
AsherMoshav <asher@mediadome.co.il>
2018-01-13 16:23:35 +0200

    Azure SSO resource id decode unit tests added

--- unitTestsAPI/tests/ssoAuthUT.js
+++ unitTestsAPI/tests/ssoAuthUT.js
@@ -16,7 +16,7 @@
  * is strictly forbidden unless prior written permission is obtained
  * from Rev Software, Inc.
  */
-require('should');
+var should = require('should');
 
 
 var oldEnv = process.env.NODE_ENV;
@@ -24,6 +24,7 @@ process.env.NODE_ENV = 'unitTests'; // use unit tests config
 
 var config = require('config');
 var utilsFile = require('../../lib/utilities');
+var zlib = require('zlib');
 
 var ssoTokenProperties = [
     'expirationTimestamp',
@@ -39,6 +40,7 @@ describe('Unit Test:', function () {
         });
 
         var token = config.get('azureSSOToken').toString(); // dummy token
+        var resourceId = config.get('azureSSOResourceId').toString(); // dummy resource id
 
         it('should successfully decrypt a valid SSO token', function (done) {
             var decToken = utilsFile.decodeSSOToken(token);
@@ -62,6 +64,23 @@ describe('Unit Test:', function () {
                     ssoTokenProperties.indexOf(p).should.not.equal(-1);
                 }
                 done();
+            });
+
+        it('should successfully decode resource ID',
+            function (done) {
+                utilsFile.decodeSSOResourceId(resourceId).then(function (id) {
+                    should(id).not.equal(undefined);
+                    done();
+                });
+            });
+
+            it('should match a regex expression with a decoded resource ID',
+            function (done) {
+                utilsFile.decodeSSOResourceId(resourceId).then(function (id) {
+                    var resIdRegexMatch = new RegExp('\/.*\/.*'); // verify the id is valid
+                    should(resIdRegexMatch.test(id)).equal(true);
+                    done();
+                });
             });
 
     });
