8c1c5b03443123fb57ea5a8cfe63b9ed01f11931
AsherMoshav <asher@mediadome.co.il>
2018-03-31 14:28:47 +0300

    Accounts migration script fixed

--- utils/migrate_accounts_hirearchy.js
+++ utils/migrate_accounts_hirearchy.js
@@ -41,12 +41,12 @@ users.model.find({}, function (err, usrs) {
                     console.log('User: ' + user.email);
                     console.log('User`s companyId: ' + user.companyId);
                     // if the user has multiple IDs in the companyId field
-                    var accountArray = user.companyId.split(',');
                     // split up the ids and make an array
-                    var parentAccount = accountArray[0];
+                    var accountArray = user.companyId.split(',');
                     // the first ID is the 'parent' id, save it to a var
-                    accountArray = accountArray.splice(1, accountArray.length - 1);
+                    var parentAccount = accountArray[0];
                     // remove the first 'parent' id from the array, we dont need to change it..
+                    accountArray = accountArray.splice(1, accountArray.length - 1);
                     for (var j = 0; j < accountArray.length; j++) {
                         var account = accountArray[j];
 
@@ -60,43 +60,54 @@ users.model.find({}, function (err, usrs) {
 });
 
 var updateAccount = function (id, parentID) {
-    accounts.get({ _id: parentID }, function (error, parentAccount) {
-        if (error) {
-            throw new Error(error);
-        } else if (parentAccount) {
-            if (parentAccount.parent_account_id) {
-                console.log('Account with ID `' + parentAccount.id + '` cannt be a Parent account because it has a Parent account.');
-            } else {
-                accounts.get({ _id: id }, function (error, acc) {
-                    if (error) {
-                        throw new Error(error);
-                    } else if (acc) {
-                        if (acc.parent_account_id && acc.parent_account_id !== parentID) {
-                            console.log('Account ID `' + acc.id + '` already has a parent account!');
-                            console.log('Current: `' + acc.parent_account_id + '`; New one: `' + parentID + '`');
-                        } else {
-                            // if the account already has a parent ID, leave it as is
-                            acc.parent_account_id = acc.parent_account_id || parentID;
-                            acc.account_id = acc.id;
-
-                            // run save function to save acc with new parent id
-                            accounts.update(acc, function (saveError, item) {
-                                if (saveError) {
-                                    throw new Error(saveError);
-                                } else if (item) {
-                                    console.log('Account `' + item.id + '` parent ID is set to: `' + item.parent_account_id + '`!');
-                                } else {
-                                    console.log('Cannot update account: `' + id + '`');
-                                }
-                            });
-                        }
-                    } else {
-                        console.log('Cannot get account: `' + id + '`');
-                    }
-                });
+    var flaggie = true;
+    accounts.list(function (err, accs) {
+        accs.forEach(function (testAccount) {
+            if (testAccount.parent_account_id === id) {
+                console.log('Account with ID `' + parentAccount.id + '` cant have a parent account because it is a parent account already.');
+                flaggie = false;
             }
-        } else {
-            console.log('Cannot get account ' + parentID);
+        });
+        if (flaggie) {
+            accounts.get({ _id: parentID }, function (error, parentAccount) {
+                if (error) {
+                    throw new Error(error);
+                } else if (parentAccount) {
+                    if (parentAccount.parent_account_id) {
+                        console.log('Account with ID `' + parentAccount.id + '` cannt be a Parent account because it has a Parent account.');
+                    } else {
+                        accounts.get({ _id: id }, function (error, acc) {
+                            if (error) {
+                                throw new Error(error);
+                            } else if (acc) {
+                                if (acc.parent_account_id && acc.parent_account_id !== parentID) {
+                                    console.log('Account ID `' + acc.id + '` already has a parent account!');
+                                    console.log('Current: `' + acc.parent_account_id + '`; New one: `' + parentID + '`');
+                                } else {
+                                    // if the account already has a parent ID, leave it as is
+                                    acc.parent_account_id = acc.parent_account_id || parentID;
+                                    acc.account_id = acc.id;
+
+                                    // run save function to save acc with new parent id
+                                    accounts.update(acc, function (saveError, item) {
+                                        if (saveError) {
+                                            throw new Error(saveError);
+                                        } else if (item) {
+                                            console.log('Account `' + item.id + '` parent ID is set to: `' + item.parent_account_id + '`!');
+                                        } else {
+                                            console.log('Cannot update account: `' + id + '`');
+                                        }
+                                    });
+                                }
+                            } else {
+                                console.log('Cannot get account: `' + id + '`');
+                            }
+                        });
+                    }
+                } else {
+                    console.log('Cannot get account ' + parentID);
+                }
+            });
         }
     });
 };
