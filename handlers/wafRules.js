c201dec450d50a4d3425506af6ab68c59f53cea3
AsherMoshav <asher@mediadome.co.il>
2018-04-20 13:29:11 +0300

    Fixed WAF Rules account permisisons

--- handlers/wafRules.js
+++ handlers/wafRules.js
@@ -217,6 +217,9 @@ exports.listWAFRules = function(request, reply) {
       }
     ],
     function(err, response) {
+      response = _.filter(response, function (rule) {
+        return permissionCheck.checkPermissionsToResource(request, { id: rule.account_id }, 'accounts');
+      });
       renderJSON(request, reply, err, response);
     });
 
